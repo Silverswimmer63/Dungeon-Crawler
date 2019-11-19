35dc035516a92174466d51c4cd0c38e97395a543
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-19 09:19:54 -0500

    asd

--- weapon.js
+++ weapon.js
@@ -2,7 +2,6 @@ class Weapon extends Item {
   constructor(name, icon, value, damage, desc) {
     super(name, icon, value, desc);
     this._damage = damage;
-
   }
   get damage(){
     return this._damage;
