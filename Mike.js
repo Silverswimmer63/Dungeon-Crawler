bec15029b793fc702683593309747bf264560926
partial <dan.dj@yahoo.com>
2019-11-20 10:52:00 -0500

    grammer fix
    i think

--- Mike.js
+++ Mike.js
@@ -79,63 +79,63 @@ icon: "L",
 range: 4,
 damage: {min:26, max:28}
 }
-{name: “Trained falcon”,
+{name: "Trained falcon",
 type: Ranged,
 Value: 10,
-desc: “I Guess he likes you”,
-Icon: “L”,
+desc: "I Guess he likes you",
+Icon: "L",
 Range: 5,
 Damage: {min:5, max:6}
 }
-{name: “Magnetic rocks”,
+{name: "Magnetic rocks",
 type: Ranged,
 Value: 8,
-Desc: “there cool… fun to play with… hurts when someone throws them at you”,
-Icon: “L”,
+Desc: "there cool… fun to play with… hurts when someone throws them at you",
+Icon: "L",
 Range: 4,
 Damage: {min:3, max:6}
 }
-{name: “Rusty revolver”,
+{name: "Rusty revolver",
 type: Ranged,
 Value: 20,
-Desc: “Please just sell it, its unsafe to use every time you pull the trigger there a good chance you'll just blow your hand off”,
-Icon: “L”,
+Desc: "Please just sell it, its unsafe to use every time you pull the trigger there a good chance you'll just blow your hand off",
+Icon: "L",
 Range: 6,
 Damage: {min:8, max:12}
 }
-{name: “Throwing Axe”,
+{name: "Throwing Axe",
 type: Ranged,
 Value: 24,
-Desc: “while normal throw axes are a quarter of half size of your arm but these are thesizeof full on battle axe”,
-Icon: “L”,
+Desc: "while normal throw axes are a quarter of half size of your arm but these are thesizeof full on battle axe",
+Icon: "L",
 Range: 2,
 Damage: {min:16, max:24}
 }
-{name: “shootgun”,
+{name: "shootgun",
 type: Ranged,
 Value: 26,
-Desc: “a shot gun but longer”,
-Icon: “L”,
+Desc: "a shot gun but longer",
+Icon: "L",
 Range: 3,
 Damage: {min:20, max:26},
 }
-{name: “Snake lasso”,
+{name: "Snake lasso",
 type: Ranged,
 Value: 30,
-Desc: “Unfortunately he's been defanged but he still has some bit”,
-Icon: “L”,
+Desc: "Unfortunately he's been defanged but he still has some bit",
+Icon: "L",
 Range: 5,
 Damage: {min:24, max:26},
 }
-{name: “Money bag”,
+{name: "Money bag",
 type: Ranged,
 Value: 36,
-Desc: “A bag of gold tied to your hand kratos style”,
-Icon: “L”,
+Desc: "A bag of gold tied to your hand kratos style",
+Icon: "L",
 Range: 4,
 Damage: {min:26, max:28}
 }
-{name: "laser pointer",
+{name: "Laser pointer",
 type: Ranged,
 value: 40,
 desc: "aim for the eye for maximum effect",
@@ -143,10 +143,10 @@ icon: "L",
 range: 10,
 damage: {min:28, max:34}
 }
-{name: "hacking laptop",
+{name: "Hacking laptop",
 type: Ranged,
 value: 50,
-desc: "smashing hands on keybord... iM iN",
+desc: "smashing hands on keyboard... iM iN (use sunglasses for max effect)",
 icon: "L",
 range: 6,
 damage: {min:30, max:32}
@@ -161,33 +161,33 @@ damage: {min:28, max:34}
 }
 {name: "Fancy future rifle",
 type: Ranged,
-value:
+value: 50,
 desc: "This rifle is clearly just an AK-47 with a ton of RGB lighting everywhere",
-icon:
-range:
-damage:
+icon: "L",
+range: 8,
+damage: {min:34, max:36}
 }
-{name:
-type:
-value:
-desc:
-icon:
-range:
-damage:
+{name: "serrated fedora",
+type: Ranged,
+value: 40,
+desc: "for those reddit users who watched a little too much james bound",
+icon: "L",
+range: 3,
+damage: {min:30, max:32}
 }
-{name:
-type:
-value:
-desc:
-icon:
-range:
-damage:
+{name: "Replica mouse",
+type: Ranged,
+value: 100,
+desc: "A replica of A fabled mouse one that shakes the strongest, boldest, and bravest men to the core. this is but a recreation of mikes computer mouse",
+icon: "L",
+range: 10,
+damage: {min:50, max:60}
 }
-{name:
-type:
-value:
-desc:
-icon:
-range:
-damage:
+{name: "Mikes controller",
+type: Both,
+value: 99999999999999,
+desc: "The only thing powerful enough to even give you a chance at killing the beast",
+icon: "C",
+range: 16,
+damage: {min:200, max:250}
 }
