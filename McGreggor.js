a049032ad3706d588d2dc0889e5650aea1c0826d
Mcgregor Sanford <greegorstalinskiboi@gmail.com>
2019-11-20 09:34:18 -0500

    added to mcgregor.js

---
+++ McGreggor.js
@@ -0,0 +1,81 @@
+
+
+const COMPLETE_LOOT_LIST + [
+  {name: "Plumbus", class: Item, value: 1000000000000000,
+desc:"just a regular old plumbus",
+icon:"P"}
+
+{name:"Crunched up engergy drink", class: VenderTrash, value:2,
+desc:"A smashed and abused can of Ketamine-infused engergy,there are
+only dropletts left but its not enough to satisfy your crippiling addiction",
+icon:","}
+
+{name:"Mysterous Arrow", class: Collectible, value: 100500006,
+desc:"Legends say that if you pierce yourself with the Arrow you will achive Godlike Powers,
+or ya know die", icon:">"}
+
+{name:"elven wood dish", class: Collectible, value: 100,
+desc:"Wait elfs don't use wood for anything, What heresy is this?",
+icon:"u"}
+
+ {name:"Gomboi SP blue edition", class: Collectible, value:299,
+desc:"It all started 53 years ago, back when i was a fully grown little boy",
+icon:"B"}
+
+{name:"A copy of ,The Ancient Solitary Regin", class: VenderTrash, value:15,
+desc:"A comprhensive guide to the Mating/Breeding habits of Owls disguiesd as a young adult novel",
+icon:"n"}
+
+{name:"Shopkeeper's fanfiction", class: VenderTrash, value:29,
+desc:"A copy of one of the shopkeeper's fanfiction, it is filled with bad grammer,metaphors,and a weird substence",
+icon:"N"}
+
+{name:"TekWar", class: VenderTrash, value:20,
+desc:"A Copy of TekWar by CapStone the pinnicle of entertainment software",
+icon:"o"}
+
+{name:"dung", class: VenderTrash, value:1,
+desc:"What do you think it is",
+icon:"6"}
+
+{name:"The bee movie script",class: Collectible, value: 5000,
+desc: "Yellow Black Yellow Black Yellow Black Yellow Black Ooh Black and Yellow let's shake it up a bit",
+icon:"b"}
+
+{name:"toy knife",class: VenderTrash, value:12,
+desc: "A normal plastic toy knife, it does no damage yet somehow it was recalled because it could
+inspire hate or whatever", icon:"4"}
+
+{}
+
+{name: "BFG9000", class: Ranged,
+value:900000000000000000000000000000000,
+desc:"A BFG from Doom, how retro", icon:"L",
+range: 10,
+damage: {min:4, max:40},
+dropoff: .1}
+]
+
+
+class Item{
+  constructor(name, value, desc, icon...) {
+    this._name = name;
+}
+
+getDamage(){
+  return this._damage
+}
+
+
+
+
+const + level 1
+const + level 2
+const + level 3
+{name: class: Item
+value: desc:
+icon:     }
+
+
+
+[ { }
