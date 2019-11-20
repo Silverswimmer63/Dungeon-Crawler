6e3d4bbd09b0fc4464c125d52faa075b61f52cce
partial <dan.dj@yahoo.com>
2019-11-20 09:40:54 -0500

    added to mike.js

---
+++ Mike.js
@@ -0,0 +1,137 @@
+Const COMPLETE_LOOT_LIST = [
+{name: "Throwing stones",
+class: Ranged,
+value: 1,
+desc: "just rocks... literaly just rocks",
+icon: "L",
+range: 4,
+damage: {min:2, max:5}
+}
+{name: "Fireworks",
+class: Ranged,
+value: 5,
+desc: "whoshhhh................... BANG!",
+icon: "L",
+range: 6,
+damage: {min:3, max:8}
+}
+{name: "Greek bow",
+class: Ranged,
+value: 12,
+desc: "It's problly at least 20 times older than you",
+icon: "L",
+range: 5,
+damage: {min:6, max:8}
+}
+{name: “Trained falcon”,
+Class: Ranged,
+Value: 10,
+desc: “I Guess he likes you”
+Icon: “L”,
+Range: 5,
+Damage: {min:5, max:6}
+}
+{name: “Magnetic rocks”,
+Class: Ranged,
+Value: 8,
+Desc: “there cool… fun to play with… hurts when someone throws them at you”,
+Icon: “L”,
+Range: 4,
+Damage: {min:3, max:6}
+}
+{name: “Rusty revolver”,
+Class: Ranged,
+Value: 20,
+Desc: “Please just sell it, its unsafe to use every time you pull the trigger there a good chance you'll just blow your hand off”,
+Icon: “L”,
+Range: 6,
+Damage: {min:8, max:12}
+}
+{name: “Throwing Axe”,
+Class: Ranged,
+Value: 24,
+Desc: “while normal throw axes are a quarter of half size of your arm but these are thesizeof full on battle axe”,
+Icon: “L”,
+Range: 2,
+Damage: {min:16, max:24}
+}
+{name: “shootgun”,
+Class: Ranged,
+Value: 26,
+Desc: “a shot gun but longer”,
+Icon: “L”,
+Range: 3,
+Damage: {min:20, max:26},
+}
+{name: “Snake lasso”,
+Class: Ranged,
+Value: 30,
+Desc: “Unfortunately he's been defanged but he still has some bit”,
+Icon: “L”,
+Range: 5,
+Damage: {min:24, max:26},
+}
+{name: “Money bag”,
+Class: Ranged,
+Value: 36,
+Desc: “A bag of gold tied to your hand kratos style”,
+Icon: “L”,
+Range: 4,
+Damage: {min:26, max:28}
+
+{name: "laser pointer",
+class: Ranged,
+value: 40,
+desc: "aim for the eye for maximum effect",
+icon: "L",
+range: 10,
+damage: {min:28, max:34}
+}
+{name: "hacking laptop",
+class: "Ranged",
+value: "50",
+desc: "smashing hands on keybord... IM IN",
+icon: "L",
+range: 6,
+damage: {min:30, max:32}
+}
+{name:""
+class:
+value:
+desc:
+icon:
+range:
+damage:
+}
+{name:
+class:
+value:
+desc:
+icon:
+range:
+damage:
+}
+{name:
+class:
+value:
+desc:
+icon:
+range:
+damage:
+}
+{name:
+class:
+value:
+desc:
+icon:
+range:
+damage:
+}
+{name:
+class:
+value:
+desc:
+icon:
+range:
+damage:
+}
