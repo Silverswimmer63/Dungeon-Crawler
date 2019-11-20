95f27a51914ebcbd5896dde77bdbbfadd0c13d9c
Lilla <dilillahall0802@gmail.com>
2019-11-20 09:25:45 -0600

    my

---
+++ Dililla.js
@@ -0,0 +1,66 @@
+const COMPLETE_ARMOR_LIST= [
+  {
+    name: "Leather Legs",
+    class: Item,
+    value: 2,
+    desc:"Low damege protection",
+    icon:"<",
+    damageresist: ,
+  }
+  {
+    name: "ChainMail Legs",
+    class: Item,
+    value: 5,
+    desc:"higher damege protection then Leather",
+    icon:"<",
+      damage: {min: 4, max:30},
+  }
+  {
+    name: "Copper Legs",
+  class: Item,
+  value: 10,
+  desc:"higher damege protection then ChainMail",
+  icon:"<",
+    damage: {min: 4, max:30},
+}
+{
+  name: "Bronze Legs",
+class: Item,
+value: 15,
+desc:"higher damege protection then Copper",
+icon:"<",
+  damage: {min: 4, max:30},
+}
+{
+  name: "Iron Legs",
+class: Item,
+value: 20,
+desc:"higher damege protection then Bronze",
+icon:"<",
+  damage: {min: 4, max:30},
+}
+{
+  name: "Steel Legs",
+class: Item,
+value: 25,
+desc:"higher damege protection then Iron",
+icon:"<",
+  damage: {min: 4, max:30},
+}
+{
+  name: "Silver Legs",
+class: Item,
+value: 33,
+desc:"higher damege protection then Steel",
+icon:"<",
+  damage: {min: 4, max:30},
+}
+{
+  name: "Platinum Legs",
+class: Item,
+value: 42,
+desc:"Max damege protection",
+icon:"<",
+  damage: {min: 4, max:30},
+}
+]
