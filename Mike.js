b668d17fe142f5b5130fb872edca65d7d36a2c23
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-20 09:50:56 -0500

    Fixed the google docs bs

--- Mike.js
+++ Mike.js
@@ -1,6 +1,6 @@
-Const COMPLETE_LOOT_LIST = [
+const cOMPLETE_LOOT_LiST = [
 {name: "Throwing stones",
-class: Ranged,
+class: ranged,
 value: 1,
 desc: "just rocks... literaly just rocks",
 icon: "L",
@@ -8,7 +8,7 @@ range: 4,
 damage: {min:2, max:5}
 }
 {name: "Fireworks",
-class: Ranged,
+class: ranged,
 value: 5,
 desc: "whoshhhh................... BANG!",
 icon: "L",
@@ -16,71 +16,71 @@ range: 6,
 damage: {min:3, max:8}
 }
 {name: "Greek bow",
-class: Ranged,
+class: ranged,
 value: 12,
-desc: "It's problly at least 20 times older than you",
+desc: "it's problly at least 20 times older than you",
 icon: "L",
 range: 5,
 damage: {min:6, max:8}
 }
-{name: “Trained falcon”,
-Class: Ranged,
-Value: 10,
-desc: “I Guess he likes you”
-Icon: “L”,
-Range: 5,
-Damage: {min:5, max:6}
+{name: "Trained falcon",
+class: ranged,
+value: 10,
+desc: "i Guess he likes you",
+icon: "L",
+range: 5,
+damage: {min:5, max:6}
 }
-{name: “Magnetic rocks”,
-Class: Ranged,
-Value: 8,
-Desc: “there cool… fun to play with… hurts when someone throws them at you”,
-Icon: “L”,
-Range: 4,
-Damage: {min:3, max:6}
+{name: "Magnetic rocks",
+class: ranged,
+value: 8,
+desc: "there cool… fun to play with… hurts when someone throws them at you",
+icon: "L",
+range: 4,
+damage: {min:3, max:6}
 }
-{name: “Rusty revolver”,
-Class: Ranged,
-Value: 20,
-Desc: “Please just sell it, its unsafe to use every time you pull the trigger there a good chance you'll just blow your hand off”,
-Icon: “L”,
-Range: 6,
-Damage: {min:8, max:12}
+{name: "rusty revolver",
+class: ranged,
+value: 20,
+desc: "Please just sell it, its unsafe to use every time you pull the trigger there a good chance you'll just blow your hand off",
+icon: "L",
+range: 6,
+damage: {min:8, max:12}
 }
-{name: “Throwing Axe”,
-Class: Ranged,
-Value: 24,
-Desc: “while normal throw axes are a quarter of half size of your arm but these are thesizeof full on battle axe”,
-Icon: “L”,
-Range: 2,
-Damage: {min:16, max:24}
+{name: "Throwing Axe",
+class: ranged,
+value: 24,
+desc: "while normal throw axes are a quarter of half size of your arm but these are thesizeof full on battle axe",
+icon: "L",
+range: 2,
+damage: {min:16, max:24}
 }
-{name: “shootgun”,
-Class: Ranged,
-Value: 26,
-Desc: “a shot gun but longer”,
-Icon: “L”,
-Range: 3,
-Damage: {min:20, max:26},
+{name: "shootgun",
+class: ranged,
+value: 26,
+desc: "a shot gun but longer",
+icon: "L",
+range: 3,
+damage: {min:20, max:26},
 }
-{name: “Snake lasso”,
-Class: Ranged,
-Value: 30,
-Desc: “Unfortunately he's been defanged but he still has some bit”,
-Icon: “L”,
-Range: 5,
-Damage: {min:24, max:26},
+{name: "Snake lasso",
+class: ranged,
+value: 30,
+desc: "Unfortunately he's been defanged but he still has some bit",
+icon: "L",
+range: 5,
+damage: {min:24, max:26},
 }
-{name: “Money bag”,
-Class: Ranged,
-Value: 36,
-Desc: “A bag of gold tied to your hand kratos style”,
-Icon: “L”,
-Range: 4,
-Damage: {min:26, max:28}
+{name: "Money bag",
+class: ranged,
+value: 36,
+desc: "A bag of gold tied to your hand kratos style",
+icon: "L",
+range: 4,
+damage: {min:26, max:28}
 
 {name: "laser pointer",
-class: Ranged,
+class: ranged,
 value: 40,
 desc: "aim for the eye for maximum effect",
 icon: "L",
@@ -88,9 +88,9 @@ range: 10,
 damage: {min:28, max:34}
 }
 {name: "hacking laptop",
-class: "Ranged",
+class: "ranged",
 value: "50",
-desc: "smashing hands on keybord... IM IN",
+desc: "smashing hands on keybord... iM iN",
 icon: "L",
 range: 6,
 damage: {min:30, max:32}
