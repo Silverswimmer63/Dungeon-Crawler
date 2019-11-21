e9202c999fae424fa48f3710fa85499984d251be
partial <dan.dj@yahoo.com>
2019-11-21 09:26:15 -0500

    more fix stuff
    fix stuff goo I do

--- Mike.js
+++ Mike.js
@@ -6,7 +6,7 @@ desc: "Just rocks... literaly just rocks",
 icon: "L",
 range: 4,
 damage: {min:2, max:5}
-}
+},
 {name: "Fireworks",
 type: Ranged,
 value: 5,
@@ -14,7 +14,7 @@ desc: "whoshhhh................... BANG!",
 icon: "L",
 range: 6,
 damage: {min:3, max:8}
-}
+},
 {name: "Greek Bow",
 type: Ranged,
 value: 12,
@@ -22,7 +22,7 @@ desc: "It's problly at least 20 times older than you",
 icon: "L",
 range: 5,
 damage: {min:6, max:8}
-}
+},
 {name: "Trained Falcon",
 type: Ranged,
 value: 10,
@@ -30,7 +30,7 @@ desc: "I guess he likes you",
 icon: "L",
 range: 5,
 damage: {min:5, max:6}
-}
+},
 {name: "Magnetic Rocks",
 type: Ranged,
 value: 8,
@@ -38,7 +38,7 @@ desc: "There cool… fun to play with… hurts when someone throws them at you",
 icon: "L",
 range: 4,
 damage: {min:3, max:6}
-}
+},
 {name: "Rusty Revolver",
 type: Ranged,
 value: 20,
@@ -46,15 +46,15 @@ desc: "Please just sell it, its unsafe to use every time you pull the trigger th
 icon: "L",
 range: 6,
 damage: {min:8, max:12}
-}
+},
 {name: "Throwing Axe",
-type: Ranged,
+type: Both,
 value: 24,
 desc: "while normal throwing axes are a quarter size of a regular axe but these are the size of a full on battle axe",
 icon: "L",
 range: 2,
 damage: {min:16, max:24}
-}
+},
 {name: "Shootgun",
 type: Ranged,
 value: 26,
@@ -62,23 +62,23 @@ desc: "A shotgun but longer",
 icon: "L",
 range: 4,
 damage: {min:20, max:26},
-}
+},
 {name: "Snake Lasso",
-type: Ranged,
+type: Both,
 value: 30,
 desc: "Unfortunately he's been defanged but he still has some bite",
 icon: "L",
 range: 5,
 damage: {min:24, max:26},
-}
+},
 {name: "Money Bag",
-type: Ranged,
+type: Both,
 value: 36,
 desc: "A bag of gold tied to your hand kratos style",
 icon: "L",
 range: 4,
 damage: {min:26, max:28}
-}
+},
 {name: "Laser Pointer",
 type: Ranged,
 value: 40,
@@ -86,7 +86,7 @@ desc: "Aim for the eye for maximum effect",
 icon: "L",
 range: 10,
 damage: {min:28, max:34}
-}
+},
 {name: "Hacking Laptop",
 type: Ranged,
 value: 50,
@@ -94,7 +94,7 @@ desc: "Smashing hands on keyboard... iM iN (use sunglasses for max effect)",
 icon: "L",
 range: 6,
 damage: {min:30, max:32}
-}
+},
 {name: "Ray Gun",
 type: Ranged,
 value: 55,
@@ -102,7 +102,7 @@ desc: "Orginated from the body of a very mad man crawling around yelling about n
 icon: "L",
 range: 6,
 damage: {min:28, max:34}
-}
+},
 {name: "Fancy Future Rifle",
 type: Ranged,
 value: 50,
@@ -110,23 +110,23 @@ desc: "This rifle is clearly just an AK-47 with a ton of RGB lighting everywhere
 icon: "L",
 range: 8,
 damage: {min:34, max:36}
-}
+},
 {name: "Serrated Fedora",
-type: Ranged,
+type: Both,
 value: 40,
 desc: "For those reddit users who watched a little too much James Bond",
 icon: "L",
 range: 3,
 damage: {min:30, max:32}
-}
+},
 {name: "Replica Mouse",
-type: Ranged,
+type: Both,
 value: 100,
 desc: "A replica of A fabled mouse one that shakes the strongest, boldest, and bravest men to the core. this is but a recreation of mikes computer mouse",
 icon: "L",
 range: 10,
 damage: {min:50, max:60}
-}
+},
 {name: "Mikes Controller",
 type: Both,
 value: 99999999999999,
@@ -135,3 +135,4 @@ icon: "C",
 range: 16,
 damage: {min:200, max:250}
 }
+]
