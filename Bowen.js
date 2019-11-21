8269bb9b73f1d285f92bef4d3f0a37180d318f5c
Bowen Zheng <bowenzheng2003@gmail.com>
2019-11-21 09:29:47 -0500

    did class extends

--- Bowen.js
+++ Bowen.js
@@ -1,64 +1,64 @@
 console.log("Bowen.js linked");
-//Body Armor list
-const COMPLETE_ARMOR_BODY_LIST = [
+//Body BodyArmor list
+const COMPLETE_BodyArmor_BODY_LIST = [
   {name: "Leather",
-  type: Armor,
+  type: BodyArmor,
   value: 5,
-  desc: "This body armor protects you from basic, low damage attacks",
+  desc: "This body BodyArmor protects you from basic, low damage attacks",
   icon: "&",
   damageResist:.1
 },
   {name: "Chainmail",
-  type: Armor,
+  type: BodyArmor,
   value: 10,
-  desc: "A better suit of armor that protects you slightly more from attacks.",
+  desc: "A better suit of BodyArmor that protects you slightly more from attacks.",
   icon: "&",
   damageResist:.2
   },
   {name: "Copper",
-  type: Armor,
+  type: BodyArmor,
   value: 15,
   desc: "This is a stronger upgrade and is better at resisting small weapona attacks",
   icon: "&",
   damageResist:.3
   },
   {name: "Bronze",
-  type: Armor,
+  type: BodyArmor,
   value: 20,
   desc: "",
   icon: "&",
   damageResist:.4
   },
   {name: "Iron",
-  type: Armor,
+  type: BodyArmor,
   value: 25,
   desc: "This protects you from medium-level attacks",
   icon: "&",
   damageResist:.5
   },
   {name: "Steel",
-  type: Armor,
+  type: BodyArmor,
   value: 30,
   desc: "A better upgrade that protects you from aggressive attacks",
   icon: "&",
   damageResist:.6
   },
   {name: "Silver",
-  type: Armor,
+  type: BodyArmor,
   value: 40,
   desc: "A better upgrade that protects you for heavy aggressive attacks",
   icon: "&",
   damageResist:.7
   },
   {name: "Platinum",
-  type: Armor,
+  type: BodyArmor,
   value: 50,
-  desc: "This is the best suit of armor on the market that you can buy/get",
+  desc: "This is the best suit of BodyArmor on the market that you can buy/get",
   icon: "&",
   damageResist:.8
   }
 ]
-class BodyArmor {
+class BodyArmor extends Item {
   constructor(name, icon, value, desc, damageResist) {
     this._name = name;// the this keywodr means thet it is something the class owns
     this._icon = icon;
