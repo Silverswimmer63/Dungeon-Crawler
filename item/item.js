aef688907bb9f9dc3371229177cef8b6338a3d43
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-21 09:29:51 -0500

    tried to fix error

--- item/item.js
+++ item/item.js
@@ -21,6 +21,8 @@ class Item {
     this._desc = desc; // the description of the item
     this._value = value;
     this._type = type; // if the item is in a collection or trash
+    this._damageresist = damageresist;
+
   }
   /**************************************** Getter and Setters *********************************/
   get name(){ return this._name; }
@@ -37,6 +39,9 @@ class Item {
 
   get type(){ return this._value; }
   set type(type){ this._type = type; }
+
+  get damageresist(){ return this._damageresist; }
+  set damageresist(damageresist){ this._damageresist = damageresist ;}
 
   toString(){
     var retString = this.name + "<br>";
