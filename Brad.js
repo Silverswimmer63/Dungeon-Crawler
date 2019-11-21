e2e2d37b8097fdb10e838f8aa51ad69bd4a2e7a8
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-21 09:35:29 -0500

    gevklbjlagsLAFHUGHVMSBZ ,NKJLIBOFvlgjk<nMD

--- Brad.js
+++ Brad.js
@@ -1,49 +1,23 @@
-<<<<<<< HEAD
-const COMPLETE_MELEE_LIST = [
-=======
 const COMPLETE_Melee_LIST = [
->>>>>>> 74d243957b46f87ffa7f92f1e2647a207a7e2277
   {name: "Fists",
   type: Melee,
   value: 0,
   desc: "The only weapon a man needs",
   damage: 5,
-<<<<<<< HEAD
-  icon: "/"
-}
-=======
   icon: "/"},
 
->>>>>>> 74d243957b46f87ffa7f92f1e2647a207a7e2277
   {name: "Boxing Gloves",
   type: Melee,
   value: 5,
   desc: "Just a littel pertection for the hands",
   damage: 10,
-<<<<<<< HEAD
-  icon:"/"
-}
-=======
   icon:"/"},
 
->>>>>>> 74d243957b46f87ffa7f92f1e2647a207a7e2277
   {name: "Axe",
   type: Melee,
   value: 50,
   desc: "A powerful weapon, used by those with great strength",
   damage: 50,
-<<<<<<< HEAD
-  icon: "/"
-}
-  {name: "Butcher Knife",
-  type: Melee,
-  value: 14,
-  desc: "A small blade, one that can be concealed,
-  but can still inflict lots of damage",
-  damage: 18,
-  icon:"/"
-}
-=======
   icon: "/"},
 
   {name: "Butcher Knife",
@@ -53,53 +27,33 @@ const COMPLETE_Melee_LIST = [
   damage: 18,
   icon:"/"},
 
->>>>>>> 74d243957b46f87ffa7f92f1e2647a207a7e2277
   {name: "Broad Sword",
   type: Melee,
   value: 34,
   desc: "A large blade, only those truely skilled have the ability to weild it properly",
   damage: 40,
-<<<<<<< HEAD
-  icon:"/"
-}
-=======
   icon:"/"},
 
->>>>>>> 74d243957b46f87ffa7f92f1e2647a207a7e2277
   {name: "Stick",
   type: Melee,
   value: 2,
   desc: "Just a normal stick , that you picked up off the floor, but it'll still hurt whe hit by.",
   damage: 12,
-<<<<<<< HEAD
-  icon:"/"}
-=======
   icon:"/"},
 
->>>>>>> 74d243957b46f87ffa7f92f1e2647a207a7e2277
   {name: "Club",
   type: Melee,
   value: 25,
   desc: "A massive, unweildly weapon that only those with the mightiest strength can hold",
   damage: 32,
-<<<<<<< HEAD
-  icon:"/"
-}
-=======
   icon:"/"},
 
->>>>>>> 74d243957b46f87ffa7f92f1e2647a207a7e2277
   {name: "Short Sword",
   type: Melee,
   value: 20,
   desc: "A common blade, one that can be found in most shops, but its sharpness explains its commoness",
   damage: 24,
-<<<<<<< HEAD
-  icon:"/"
-}
-=======
   icon:"/"}
->>>>>>> 74d243957b46f87ffa7f92f1e2647a207a7e2277
 ]
 
 class Melee extends Item{
