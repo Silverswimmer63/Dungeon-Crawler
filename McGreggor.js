b7f60328837914c973a73f402b15389f79dda9c1
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-22 09:55:49 -0500

    speshiac

--- McGreggor.js
+++ McGreggor.js
@@ -1,14 +1,14 @@
 
 
 const COMPLETE_LOOT_LIST = [
-{name:"Crunched up energy drink", class: VenderTrash, value:2,
-desc:"A smashed and abused can of Ketamine-infused energy. There are
-only droplets left but it is not enough to satisfy your crippling addiction",
+{name:"Crunched up energy drink",
+class: Vendortrash,
+value:2,
+desc: "A smashed and abused can of Ketamine-infused energy. There are only droplets left but it is not enough to satisfy your crippling addiction",
 icon:"C"},
 
 {name:"Mysterious Arrow", class: Collectible, value: 100500006,
-desc:"Legends say that if you pierce yourself with the Arrow you will achive God-like Powers,
-or die", icon:"C"},
+desc:"Legends say that if you pierce yourself with the Arrow you will achive God-like Powers, or die", icon:"C"},
 
 {name:"elven wood dish", class: Collectible, value: 100,
 desc:"Wait elves don't use wood for anything. What heresy is this?",
@@ -18,49 +18,41 @@ icon:"C"},
 desc:"It all started 53 years ago, back when I was a fully grown little boy",
 icon:"C"},
 
-{name:"A copy of The Ancient Solitary Reign", class: VenderTrash, value:15,
+{name:"A copy of The Ancient Solitary Reign", class: Vendortrash, value:15,
 desc:"A comprehensive guide to the Mating/Breeding habits of Owls disguised as a young adult novel",
 icon:"V"},
 
-{name:"Shopkeeper's fanfiction", class: VenderTrash, value:29,
+{name:"Shopkeeper's fanfiction", class: Vendortrash, value:29,
 desc:"A copy of one of the shopkeeper's fanfiction. It is filled with bad grammer, metaphors,and a weird substance",
 icon:"V"},
 
-{name:"TekWar", class: VenderTrash, value:20,
+{name:"TekWar", class: Vendortrash, value:20,
 desc:"A Copy of TekWar by CapStone: the pinnicle of entertainment software",
 icon:"V"},
 
-{name:"dung", class: VenderTrash, value:1,
+{name:"dung", class: Vendortrash, value:1,
 desc:"What do you think it is?",
 icon:"V"},
 
-{name:"The bee movie script",class: Collectible, value: 5000,
+{name:"The bee movie script",
+class: Collectible,
+value: 5000,
 desc: "Yellow Black Yellow Black Yellow Black Yellow Black Ooh Black and Yellow let's shake it up a bit",
 icon:"C"},
 
-{name:"toy knife",class: VenderTrash, value:12,
-desc: "A normal plastic toy knife. It does no damage yet somehow it was recalled because it could
-inspire hate or whatever", icon:"V"},
+{name:"toy knife",
+class: Vendortrash,
+value:12,
+desc: "A normal plastic toy knife. It does no damage yet somehow it was recalled because it could inspire hate or whatever", icon:"V"},
 
-{name:"Mike's Mouse's trophy", class:Endgame Collectible, vlaue:384107401847017409308758173587130870775014,
-desc:"You did it! you seduced a giant frackin rat, we hope you to have a long married life with lots of
-inbred human rat hybrids", icon:"C"},
+{name:"Mike's Mouse's trophy",
+class:Vendortrash,
+vlaue:384107401847017409308758173587130870775014,
+desc:"You did it! you seduced a giant frackin rat, we hope you to have a long married life with lots of inbred human rat hybrids", icon:"C"},
 
-{name:"The Mona Lisa", class: Collectible, value: 100000000,
+{name:"The Mona Lisa",
+class: Collectible,
+value: 100000000,
 desc:"The first time I saw her with hands on her knee HOW SHOULD I SAY THIS, I had a B@#&!)",
 icon:"C"}
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
 ]
