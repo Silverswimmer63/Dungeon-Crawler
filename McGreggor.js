90bed5f8a2cdfea0c114d5c8c7440c20a91f2793
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-22 10:29:13 -0500

    NO MORE ERRORS

--- McGreggor.js
+++ McGreggor.js
@@ -76,20 +76,20 @@ icon:"C"}
 const COMPLETE_POTION_LIST = [
   {name:"Molotov Milk", class:Potion, value:20,
 desc:"I AM THE MILK MAN. MY MILK IS DELICOUS, SPECIAL DELEVERY TODAY",
-icon:"P", selfother:Other, damage:{min:1, mx:15}},
+icon:"P", damage:{min:1, mx:15}},
 
 {name:"lemon's aid", class:Potion, value:10,
 desc:"A type of soda made specifically for medical purposes not for fun, that means you Jimbo",
-icon:"P", selfother:Self, heal:{min:1, mx:20}},
+icon:"P", heal:{min:1, mx:20}},
 
 {name:"SS13 Cognac", class:Potion, value:100,
-desc:"A form a space beer created in space for the memory of all the brave spessmen and women who gave thier lives on space station thirteen, may their souls protect you", icon:"P", selfother:Self, heal:{min:30, mx:45}},
+desc:"A form a space beer created in space for the memory of all the brave spessmen and women who gave thier lives on space station thirteen, may their souls protect you", icon:"P", heal:{min:30, mx:45}},
 
 {name:"Mon-star energy", class:Potion, value:2,
-desc:"That's right ladies and gentlemen, we sold out to give you some Zoomer juice", icon:"P", selfother:Self,
+desc:"That's right ladies and gentlemen, we sold out to give you some Zoomer juice", icon:"P",
 heal:{min:2, mx:7}},
 
 {name:"Boingo oingo Botion", class:Potion, value:120,
-desc:"A weird rainbow colored concoction that gives of a scent of pure diabetes",icon:"P", selfother:Other,
+desc:"A weird rainbow colored concoction that gives of a scent of pure diabetes",icon:"P",
 damage:{min:20, mx:45}},
 ]
---
+++ item/usuables/potions/potions.js
@@ -0,0 +1,5 @@
+class Potion extends Item {
+  constructor(name, icon, desc, value, type){
+    super(name, icon, desc, value, type);
+  }
+}
