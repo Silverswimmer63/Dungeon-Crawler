e93b08537b71c93c046f8c4414d60c7ed096c532
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-22 09:50:31 -0500

    merge crap

--- item/usuables/weapons/ranged/ranged.js
+++ item/usuables/weapons/ranged/ranged.js
@@ -1,12 +1,7 @@
 
 class Ranged extends Weapon {
-<<<<<<< HEAD
-  constructor(name, icon, value, damage, desc, range) {
-    super(name, icon, value, damage, desc);
-=======
   constructor (name, icon, value, desc, damage, range){
   super (name, icon, value, desc, damage, range;
->>>>>>> 72bc656bd6029d4378f1e111333e6f5432eb040a
     this._range = range;
 }
 get range(){ return this._range; }
--- item/usuables/weapons/weapons.js
+++ item/usuables/weapons/weapons.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Weapon extends Item{
   constructor(name, icon, desc, value, type, damage){
     super(name, icon, desc, value, type)
@@ -6,13 +5,4 @@ class Weapon extends Item{
   }
 get damage() { return this._damage; }
 set damage(damage) { this._damage = damage; }
-=======
-class Weapon extends Item {
-  constuctor (name, type, value, desc, icon){
-  super(name, type, value, desc, icon, damage,)
-  this._damage = damage;
-}
-  get damage() { return this._damage; }
-  set damage(damage) { this._damage = damage; }
->>>>>>> 72bc656bd6029d4378f1e111333e6f5432eb040a
 }
