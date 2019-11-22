9d64566abf271db9826d74e434fce008b79e3aee
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-22 09:51:16 -0500

    fixed the issue in ranged

--- item/usuables/weapons/ranged/ranged.js
+++ item/usuables/weapons/ranged/ranged.js
@@ -1,7 +1,7 @@
 
 class Ranged extends Weapon {
   constructor (name, icon, value, desc, damage, range){
-  super (name, icon, value, desc, damage, range;
+  super (name, icon, value, desc, damage, range);
     this._range = range;
 }
 get range(){ return this._range; }
