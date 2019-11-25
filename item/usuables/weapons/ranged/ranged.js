423eafdec34042a9093026976beed66230a37b1a
partial <dan.dj@yahoo.com>
2019-11-25 09:25:18 -0500

    Auto stash before merge of "master" and "origin/master"

--- item/usuables/weapons/ranged/ranged.js
+++ item/usuables/weapons/ranged/ranged.js
@@ -1,4 +1,5 @@
 
+<<<<<<< Updated upstream
 class Ranged extends Weapon {
   constructor (name, icon, value, desc, damage, range){
   super (name, icon, value, desc, damage, range);
@@ -7,3 +8,48 @@ class Ranged extends Weapon {
 get range(){ return this._range; }
 set range(range){ this._range = range; }
 }
+=======
+  get value(){return this._value; }
+  set value(value) {this._value = value; }
+
+  get icon(){return this._icon; }
+  set icon(icon) {this._icon = icon; }
+
+  get damage(){return this._damage; }
+  set damage(damage) {this._damage = damage; }
+
+  get desc(){return this._desc; }
+  set desc(desc) {this._name = desc; }
+
+    get range(){return this._range; }
+    set range(range) {this._range = range; }
+
+class Weapon extends Item {
+  constructor(name, icon, value, damage, desc, range) {
+    super(name, icon, value, desc);
+    this._type = Both;
+    this._name = name;
+    this._icon = icon;
+    this._value = value;
+    this._damage = damage;
+    this._desc = desc;
+    this._range = range;
+  }
+  get name(){return this._name; }
+  set name(name) {this._name = name; }
+
+  get value(){return this._value; }
+  set value(value) {this._value = value; }
+
+  get icon(){return this._icon; }
+  set icon(icon) {this._icon = icon; }
+
+  get damage(){return this._damage; }
+  set damage(damage) {this._damage = damage; }
+
+  get desc(){return this._desc; }
+  set desc(desc) {this._name = desc; }
+
+    get range(){return this._range; }
+    set range(range) {this._range = range; }
+>>>>>>> Stashed changes
