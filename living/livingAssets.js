d4f88ecd850112eb9077d15f443ad5a39de6f24d
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-26 10:17:55 -0500

    fixed the spacind in livingAssets.js

--- living/livingAssets.js
+++ living/livingAssets.js
@@ -26,41 +26,46 @@ const COMPLETE_MythologicalIN_LIST= [
 ]
 
 const COMPLETE_No_MythologicalIN_LIST= [
-  {name:"Gremlin",
-  hp: 10,
-  attackDam:{min:5, max:10},
-  icon:"m",
-  desc: "little green monsters as common as rats in the city honestly mostly just an annoyance more than anything",
-  type: NoMythological,
-},
-{name:"lemures",
-hp:10,
-attackDam:{min:5, max:10},
-icon:"m",
-desc:"dark spirits that look like ghost emmos",
-type:NoMythological,
-},
-{name:"Azeban",
-hp: 10,
-attackDam:{min:5, max:10},
-icon:"m",
-desc:"imagine rocket from guardians of the galaxy but as a poor theff",
-type:NoMythological,
-},
-{name:"Minitar",
-hp: 70,
-attackDam:{min:20, max:40},
-icon:"M",
-desc:"He's a sweetheart if you get to know him",
-type:NoMythological,
-},
-{name:"Areas",
-hp: 100,
-attackDam:{min:30, max:54},
-icon:"A",
-desc:"He's always mad because he has daddy issues",
-type:NoMythological,
-}
+  {
+    name:"Gremlin",
+    hp: 10,
+    attackDam:{min:5, max:10},
+    icon:"m",
+    desc: "little green monsters as common as rats in the city honestly mostly just an annoyance more than anything",
+    type: NoMythological,
+  },
+  {
+    name:"lemures",
+    hp:10,
+    attackDam:{min:5, max:10},
+    icon:"m",
+    desc:"dark spirits that look like ghost emmos",
+    type:NoMythological,
+  },
+  {
+    name:"Azeban",
+    hp: 10,
+    attackDam:{min:5, max:10},
+    icon:"m",
+    desc:"imagine rocket from guardians of the galaxy but as a poor theff",
+    type:NoMythological,
+  },
+  {
+    name:"Minitar",
+    hp: 70,
+    attackDam:{min:20, max:40},
+    icon:"M",
+    desc:"He's a sweetheart if you get to know him",
+    type:NoMythological,
+  },
+  {
+    name:"Areas",
+    hp: 100,
+    attackDam:{min:30, max:54},
+    icon:"A",
+    desc:"He's always mad because he has daddy issues",
+    type:NoMythological,
+  }
 ]
 
 const COMPLETE_Cowboy_LIST = [
@@ -71,6 +76,7 @@ const COMPLETE_Cowboy_LIST = [
     desc: "He wants to take all of you possessions: your money, your clothes, and your mouse",
     icon: "T",
     attackDam: {max: 30, min:20},
+    drop: "",
   },
   {
     name: "Drunk Man",
@@ -79,6 +85,7 @@ const COMPLETE_Cowboy_LIST = [
     desc: "He may be drunk but he can still kill you because the strength of drunk people is unmatched",
     icon: "T",
     attackDam: {max: 30, min:20},
+    drop: "",
   },
   {
     name: "Outlaw",
@@ -87,50 +94,56 @@ const COMPLETE_Cowboy_LIST = [
     desc: "Will do anything to get anything",
     icon: "T",
     attackDam: {max: 30, min:20},
+    drop: "",
   }
 ]
 
 const COMPLETE_NoCowboy_LIST = [
-{name:"Kactis Karen",
-type:Nocowboy,
-hp:10,
-desc:"She's come to take the kids,but you don't have kids so she wants to speak with you're manager, but youre also not imployed so she wants to kill you instead",
-icon:"M",
-attackDamage:15},
-
-{name:"snek",
-type:Nocowboy,
-hp:5,
-desc:"I hate sneks",
-icon:"M",
-attackDamage:10},
-
-{name:"drunken demolition specilist",
-type:Nocowboy,
-hp:15,
-desc:"GonNa Kill you aNa Keep kiLliN yoU and...WhAt?",
-icon:"M",
-attackDam:20},
-
-{name:"Angry cow",
-type:Nocowboy,
-hp:10,
-Desc:"(A series of angry moos that corilate unto one word)...Moo!",
-attackDam:25,
-icon:"M"},
-
-{name:"Bandit Bandito",
-type:Nocowboy,
-hp:25,
-desc:"Texas red's gonna whip your cream",
-attackDam:20,
-icon:"M"}
-
+  {
+    name:"Kactis Karen",
+    type:Nocowboy,
+    hp:10,
+    desc:"She's come to take the kids,but you don't have kids so she wants to speak with you're manager, but youre also not imployed so she wants to kill you instead",
+    icon:"M",
+    attackDamage:15
+  },
+  {
+    name:"snek",
+    type:Nocowboy,
+    hp:5,
+    desc:"I hate sneks",
+    icon:"M",
+    attackDamage:10
+  },
+  {
+    name:"drunken demolition specilist",
+    type:Nocowboy,
+    hp:15,
+    desc:"GonNa Kill you aNa Keep kiLliN yoU and...WhAt?",
+    icon:"M",
+    attackDam:20
+  },
+  {
+    name:"Angry cow",
+    type:Nocowboy,
+    hp:10,
+    Desc:"(A series of angry moos that corilate unto one word)...Moo!",
+    attackDam:25,
+    icon:"M"
+  },
+  {
+    name:"Bandit Bandito",
+    type:Nocowboy,
+    hp:25,
+    desc:"Texas red's gonna whip your cream",
+    attackDam:20,
+    icon:"M"
+  }
 ]
 
-
   const COMPLETE_Inventory_SPACE_LIST = [
-    {name: "Robots",
+  {
+    name: "Robots",
     type: Space,
     hp: 60,
     desc: "All generic futuristic space video games have untamable robots, so why we do the same thing. These are deadly bois that are fun to fight.",
@@ -138,7 +151,8 @@ icon:"M"}
     attackDam: 50,
     drop: "Chainmail Armor"
   },
-    {name: "Knife Roomba",
+  {
+    name: "Knife Roomba",
     type: Space,
     hp: 60,
     desc: "In this world, Roombas are tired of cleaning up things that humans are too lazy to do. Now these bloodthirsty are ready to spill your blood and then clean it up.",
@@ -146,7 +160,8 @@ icon:"M"}
     attackDam: 50,
     drop: "Copper Armor"
   },
-    {name: "Self-Driving Cars",
+  {
+    name: "Self-Driving Cars",
     type: Space,
     hp: 60,
     desc: "That classic, beloved Vroom-Vroom noise becomes the stuff of nightmares in this dystopian universe. You better watch out since they're stopping for no one.",
@@ -154,7 +169,8 @@ icon:"M"}
     attackDam: 50,
     drop: "Bronze Armor"
   },
-    {name: "Steve Jobs", type: Space,
+  {
+    name: "Steve Jobs", type: Space,
     hp: 160,
     desc: "Our beloved iPhone and Windows creator is bringing back something more than a new line of the same iPhone and Windows tech support: vengeance and an unquenchable thirst for blood. But mostly vengeance since you're not buying his insanely high priced iPhones or install the Windows 10 when we clearly ask you to update and restart your computer without closing the computer.",
     icon: "$",
@@ -164,35 +180,36 @@ icon:"M"}
   ]
 
   const COMPLETE_Nospace_LIST = [
-    {name: "Androids",
+  {
+    name: "Androids",
     type: Nospace,
     hp: 50,
     desc: "Dudes that look like terminator, but not as buff.",
     icon: "$",
     attackDam: {min: 35, max:45},
   },
-  {name: "Rouge Alexa",
-  type: Nospace,
-  hp: 50,
-  desc: "I knew that Alexa would become the supreme leaders someday.",
-  icon: "$",
-  attackDam: {min: 35, max:45},
-},
-
-{name: "Cyber Bully",
-type: Nospace,
-hp: 50,
-desc: "I imagined him to be a lot bigger in person.",
-icon: "$",
-attackDam: {min: 35, max: 45},
-},
-
-{name: "Tech Support",
-type: Nospace,
-hp: 130,
-desc: "Cannot actually help you, just keeps you on hold for an hour.",
-icon: "$",
-attackDam: {min: 35, max: 45},
-}
-
+  {
+    name: "Rouge Alexa",
+    type: Nospace,
+    hp: 50,
+    desc: "I knew that Alexa would become the supreme leaders someday.",
+    icon: "$",
+    attackDam: {min: 35, max:45},
+  },
+  {
+    name: "Cyber Bully",
+    type: Nospace,
+    hp: 50,
+    desc: "I imagined him to be a lot bigger in person.",
+    icon: "$",
+    attackDam: {min: 35, max: 45},
+  },
+  {
+    name: "Tech Support",
+    type: Nospace,
+    hp: 130,
+    desc: "Cannot actually help you, just keeps you on hold for an hour.",
+    icon: "$",
+    attackDam: {min: 35, max: 45},
+  }
 ]
