8e2d12c8cb38b9a9aad42187d65f8c1533c9d20f
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-26 10:03:58 -0500

    moved the lists to itemAssets.js

--- Bowen.js
+++ Bowen.js
@@ -1,63 +1,6 @@
 console.log("Bowen.js linked");
 // Body list
-const COMPLETE_ARMOR_BODY_LIST = [
-  {name: "Leather",
-  type: Body,
-  value: 5,
-  desc: "The most basic level armor that all beginners should have. Moo-moo cows have died for this sole purpose of protecting you!",
-  icon: "&",
-  damageResist:.1
-},
-  {name: "Chainmail",
-  type: Body,
-  value: 10,
-  desc: "When we designed this unique piece of armor, we thought 'Why not copy Minecraft?'.' Oh yeah, this also protects you better than moo-moo cows, I guess",
-  icon: "&",
-  damageResist:.2
-  },
-  {name: "Copper",
-  type: Body,
-  value: 15,
-  desc: "This was forged the fiery forges of Hephaestus. He's the God of fire and he's good at building stuff I guess. Note: No Hephaestusus was harmed in the process.",
-  icon: "&",
-  damageResist:.3
-  },
-  {name: "Bronze",
-  type: Body,
-  value: 20,
-  desc: "We have two words for you: Copper and Tin. The heavenly blend of these 2 metal elements gives you a more of a luxury to protect yourself. Now you can tell monsters to watch out.",
-  icon: "&",
-  damageResist:.4
-  },
-  {name: "Iron",
-  type: Body,
-  value: 25,
-  desc: "Despite being the most common element on Earth, stronger opponents will cower in fear (at first but then attack you, but you're good since you're using iron. Right?)",
-  icon: "&",
-  damageResist:.5
-  },
-  {name: "Steel",
-  type: Body,
-  value: 30,
-  desc: "Why use steel for swords and weapons when you can use it for armor. Being the capitalists, I mean, innovators that we are, we brought you the best of worlds: Iron and Carbon",
-  icon: "&",
-  damageResist:.6
-  },
-  {name: "Silver",
-  type: Body,
-  value: 40,
-  desc: "Silver is good for trading. But it's also good for flexing on other opponenets your high status when you purchase this. Oh and it's good for defending too.",
-  icon: "&",
-  damageResist:.7
-  },
-  {name: "Platinum",
-  type: Body,
-  value: 50,
-  desc: "We are running out of ideas and metals to exploit so we settled on our last, exciting, better armor. Platinum brings you the highest form of comfort, protection, and durability. Hey kids, don't waste your money on the new iPhone 11. Spend it on this armor!",
-  icon: "&",
-  damageResist:.8
-  }
-]
+
 /*
 Robots (60 HP)
 Knife Roomba (60 HP)
