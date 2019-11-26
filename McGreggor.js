f63cf78a78e20d2602f0a11f582b6661db975f1a
Mcgregor Sanford <greegorstalinskiboi@gmail.com>
2019-11-26 09:37:53 -0500

    Added munsters

--- McGreggor.js
+++ McGreggor.js
@@ -3,25 +3,25 @@ const COMPLETE_LOOT_LIST = [
 class: VendorTrash,
 value: 2,
 desc: "A smashed and abused can of Ketamine-infused energy. There are only droplets left but it is not enough to satisfy your crippling addiction",
-icon:"V"},
+icon:"C"},
 
 {name:"Mysterious Arrow",
 class: VendorTrash,
 value: 100500006,
 desc:"Legends say that if you pierce yourself with the Arrow you will achive God-like Powers, or die",
-icon:"V"},
+icon:"C"},
 
 {name:"elven wood dish",
 class: VendorTrash,
 value: 100,
 desc:"Wait elves don't use wood for anything. What heresy is this?",
-icon:"V"},
+icon:"C"},
 
 {name:"Gomboi SP blue edition",
 class: VendorTrash,
 value: 299,
 desc:"It all started 53 years ago, back when I was a fully grown little boy",
-icon:"V"},
+icon:"C"},
 
 {name:"A copy of The Ancient Solitary Reign",
  class: VendorTrash,
@@ -51,7 +51,7 @@ icon:"V"},
 class: VendorTrash,
 value: 5000,
 desc: "Yellow Black Yellow Black Yellow Black Yellow Black Ooh Black and Yellow let's shake it up a bit",
-icon:"V"},
+icon:"C"},
 
 {name:"toy knife",
 class: VendorTrash,
@@ -63,13 +63,13 @@ icon:"V"},
 class:VendorTrash,
 value: 1000,
 desc:"You did it! you seduced a giant frackin rat, we hope you to have a long married life with lots of inbred human rat hybrids",
-icon:"V"},
+icon:"C"},
 
 {name:"The Mona Lisa",
 class: VendorTrash,
 value: 100000000,
 desc:"The first time I saw her with hands on her knee HOW SHOULD I SAY THIS, I had a B@#&!)",
-icon:"V"}
+icon:"C"}
 ]
 
 
@@ -83,7 +83,8 @@ desc:"A type of soda made specifically for medical purposes not for fun, that me
 icon:"P", heal:{min:1, mx:20}},
 
 {name:"SS13 Cognac", class:Potion, value:100,
-desc:"A form a space beer created in space for the memory of all the brave spessmen and women who gave thier lives on space station thirteen, may their souls protect you", icon:"P", heal:{min:30, mx:45}},
+desc:"A form a space beer created in space for the memory of all the brave spessmen and women who gave thier lives on space station thirteen, may their souls protect you",
+icon:"P", heal:{min:30, mx:45}},
 
 {name:"Mon-star energy", class:Potion, value:2,
 desc:"That's right ladies and gentlemen, we sold out to give you some Zoomer juice", icon:"P",
@@ -92,4 +93,31 @@ heal:{min:2, mx:7}},
 {name:"Boingo oingo Botion", class:Potion, value:120,
 desc:"A weird rainbow colored concoction that gives of a scent of pure diabetes",icon:"P",
 damage:{min:20, mx:45}},
+
+{name:"Super potion", class:Potion, value:250,
+desc:"It's super effective!", icon:"P",
+heal:{min:30, mx:55}},
+
+{name:"Cactis juice", class:Potion, value:25,
+desc:"Boys drink water, gamers drink mon-star, Real Men drink  C A C T I S  J U I C E",
+icon:"P", heal:{min:1, mx:25}}
+]
+
+const COMPLETE_MOB_LIST = [
+{name:"Kactis Karen", class:Monster, hp:10,
+desc:"She's come to take the kids,but you don't have kids so she wants to speak with you're manager,
+but youre also not imployed so she wants to kill you instead", icon:"M", attackDamage:15}
+
+{name:"snek", class:Monster, hp:5,
+desc:"I hate sneks", icon:"M", attackDamage:10}
+
+{name:"drunken demolition specilist", class:Monster, hp:15,
+desc:"GonNa Kill you aNa Keep kiLliN yoU and...WhAt?", icon:"M", attackDam:20}
+
+{name:"Angry cow", class:Monstar, hp:10,
+Desc:"(A series of angry moos that corilate unto one word)...Moo!", attackDam:25, icon:"M"}
+
+{name:"Bandit Bandito", class:Monstar, hp:25,
+desc:"Texas red's gonna whip your cream", attackDam:20, icon:"M"}
+
 ]
