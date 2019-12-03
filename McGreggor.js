c410c92e36e941cb1e517fb6f8605200d4e5c49c
Mcgregor Sanford <greegorstalinskiboi@gmail.com>
2019-12-03 09:10:02 -0500

    ack

--- McGreggor.js
+++ McGreggor.js
@@ -1,3 +1,76 @@
+const COMPLETE_LOOT_LIST = [
+{name:"Crunched up energy drink",
+class: VendorTrash,
+value: 2,
+desc: "A smashed and abused can of Ketamine-infused energy. There are only droplets left but it is not enough to satisfy your crippling addiction",
+icon:"C"},
+
+{name:"Mysterious Arrow",
+class: VendorTrash,
+value: 100500006,
+desc:"Legends say that if you pierce yourself with the Arrow you will achive God-like Powers, or die",
+icon:"C"},
+
+{name:"elven wood dish",
+class: VendorTrash,
+value: 100,
+desc:"Wait elves don't use wood for anything. What heresy is this?",
+icon:"C"},
+
+{name:"Gomboi SP blue edition",
+class: VendorTrash,
+value: 299,
+desc:"It all started 53 years ago, back when I was a fully grown little boy",
+icon:"C"},
+
+{name:"A copy of The Ancient Solitary Reign",
+ class: VendorTrash,
+ value: 15,
+ desc:"A comprehensive guide to the Mating/Breeding habits of Owls disguised as a young adult novel",
+ icon:"V"},
+
+{name:"Shopkeeper's fanfiction",
+class: VendorTrash,
+value: 29,
+desc:"A copy of one of the shopkeeper's fanfiction. It is filled with bad grammer, metaphors,and a weird substance",
+icon:"V"},
+
+{name:"TekWar",
+class: VendorTrash,
+value: 20,
+desc:"A Copy of TekWar by CapStone: the pinnicle of entertainment software",
+icon:"V"},
+
+{name:"dung",
+class: VendorTrash,
+value: 1,
+desc:"What do you think it is?",
+icon:"V"},
+
+{name:"The bee movie script",
+class: VendorTrash,
+value: 5000,
+desc: "Yellow Black Yellow Black Yellow Black Yellow Black Ooh Black and Yellow let's shake it up a bit",
+icon:"C"},
+
+{name:"toy knife",
+class: VendorTrash,
+value: 12,
+desc: "A normal plastic toy knife. It does no damage yet somehow it was recalled because it could inspire hate or whatever",
+icon:"V"},
+
+{name:"Mike's Mouse's trophy",
+class:VendorTrash,
+value: 1000,
+desc:"You did it! you seduced a giant frackin rat, we hope you to have a long married life with lots of inbred human rat hybrids",
+icon:"C"},
+
+{name:"The Mona Lisa",
+class: VendorTrash,
+value: 100000000,
+desc:"The first time I saw her with hands on her knee HOW SHOULD I SAY THIS, I had a B@#&!)",
+icon:"C"}
+]
 
 
 const COMPLETE_POTION_LIST = [
@@ -29,10 +102,8 @@ heal:{min:30, mx:55}},
 desc:"Boys drink water, gamers drink mon-star, Real Men drink  C A C T I S  J U I C E",
 icon:"P", heal:{min:1, mx:25}}
 ]
-<<<<<<< Updated upstream
-=======
 
-const COMPLETE_MOB_LIST = [
+const COMPLETE_MOB_COWBOY_LIST = [
 {name:"Kactis Karen", class:Monster, hp:10,
 desc:"She's come to take the kids,but you don't have kids so she wants to speak with you're manager,
 but youre also not imployed so she wants to kill you instead", icon:"M", attackDam:15}
@@ -49,4 +120,3 @@ Desc:"(A series of angry moos that corilate unto one word)...Moo!", attackDam:25
 {name:"Bandit Bandito", class:Monstar, hp:25,
 desc:"Texas red's gonna whip your cream", attackDam:20, icon:"M"}
 ]
->>>>>>> Stashed changes
