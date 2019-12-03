2b7ef47e34798f17aabc6aa01e5538066ab71254
Euan Barclay <Silverswimmer63@gmail.com>
2019-12-03 09:13:32 -0500

    still issues

--- McGreggor.js
+++ McGreggor.js
@@ -29,24 +29,19 @@ heal:{min:30, mx:55}},
 desc:"Boys drink water, gamers drink mon-star, Real Men drink  C A C T I S  J U I C E",
 icon:"P", heal:{min:1, mx:25}}
 ]
-<<<<<<< Updated upstream
-=======
-
 const COMPLETE_MOB_LIST = [
 {name:"Kactis Karen", class:Monster, hp:10,
-desc:"She's come to take the kids,but you don't have kids so she wants to speak with you're manager,
-but youre also not imployed so she wants to kill you instead", icon:"M", attackDam:15}
+desc:"She's come to take the kids,but you don't have kids so she wants to speak with you're manager, but youre also not imployed so she wants to kill you instead", icon:"M", attackDam:15}
 
 {name:"snek", class:Monster, hp:5,
-desc:"I hate sneks", icon:"M", attackDam:10}
+desc:"I hate sneks", icon:"M", attackDam:10},
 
 {name:"drunken demolition specilist", class:Monster, hp:15,
-desc:"GonNa Kill you aNa Keep kiLliN yoU and...WhAt?", icon:"M", attackDam:20}
+desc:"GonNa Kill you aNa Keep kiLliN yoU and...WhAt?", icon:"M", attackDam:20},
 
 {name:"Angry cow", class:Monstar, hp:10,
-Desc:"(A series of angry moos that corilate unto one word)...Moo!", attackDam:25, icon:"M"}
+Desc:"(A series of angry moos that corilate unto one word)...Moo!", attackDam:25, icon:"M"},
 
 {name:"Bandit Bandito", class:Monstar, hp:25,
 desc:"Texas red's gonna whip your cream", attackDam:20, icon:"M"}
 ]
->>>>>>> Stashed changes
