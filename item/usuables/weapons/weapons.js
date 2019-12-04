1098304fb9e71b80286d2453d51f4d304cadd87d
Euan Barclay <Silverswimmer63@gmail.com>
2019-12-04 10:17:27 -0500

    ork i has do

--- item/usuables/weapons/weapons.js
+++ item/usuables/weapons/weapons.js
@@ -1,12 +1,12 @@
 class Weapon extends Item{
-  constructor(name, icon, desc, value, type, damage){
-    super(name, icon, desc, value, type)
+  constructor(name, icon, desc, value, type, damage, text){
+    super(name, icon, desc, value, type, text)
     this._damage = damage;
   }
-get damage() { return this._damage; }
-set damage(damage) { this._damage = damage; }
+  get damage() { return this._damage; }
+  set damage(damage) { this._damage = damage; }
 
-attackDam(){
-  return Utils.randMath(this.damage.min, this.damage.max);
-  }
+  attackDam(){ return Utils.randMath(this.damage.min, this.damage.max); }
+
+  
 }
