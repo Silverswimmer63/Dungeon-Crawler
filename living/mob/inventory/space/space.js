4e8778bce63908869f8e7ee20328298ae13e3da1
Bowen Zheng <bowenzheng2003@gmail.com>
2019-12-04 10:16:50 -0500

    Added text/tostring to space and no space

--- living/mob/inventory/space/space.js
+++ living/mob/inventory/space/space.js
@@ -3,3 +3,10 @@ class Space extends Inventory{
     super(name, type, hp, desc, icon, attackDam, drop);
   }
 }
+text(){
+  var retString = "This is " + this.name + "<br>";
+  retString += "This guy is " + this.desc + "<br>";
+  retString += "He has " + this.hp + " HP.<br>";
+  retString += "His attack damage is " + this.attackDam + "<br>"
+  retString += "WHen you kill him, he drops " + this.drop + "<br>"
+  return retString;
--- living/mob/noinventory/space/no-space.js
+++ living/mob/noinventory/space/no-space.js
@@ -3,31 +3,11 @@ constructor(name, type, hp, desc, icon, attackDam){
   super(name, type, hp, desc, icon, attackDam);
 }
 }
-/*  this._name = name;
-  this._type = type;
-  this._hp = hp; // all hp is 50
-  this._desc = desc;
-  this._icon = icon; // $ will be the icon
-  this._attackDam = attackDam // min: 35, max:45
- }
-
- /****************************** Getters & Setters ****************************
-
-get name() { return this.name; }
-set name(name) { this._name = name; }
-
-get type() { return this._type; }
-set type(type) { this._type = type; }
-
-get hp() { return this._hp; }
-set hp(hp) { this._hp = hp; }
-
-get desc() { return this._desc; }
-set desc(desc) { this._desc = desc; }
-
-get icon() { return this._icon; }
-set icon() { this._icon = icon; }
-
-get attackDam() { return this._attackDam; }
-set attackDam(attackDam) {this._attackDam = attackDam; }
-} */
+text(){
+  var retString = "This is " + this.name + "<br>";
+  retString += "This guy is " + this.desc + "<br>";
+  retString += "He has " + this.hp + " HP.<br>";
+  retString += "His attack damage is " + this.attackDam + "<br>"
+  return retString;
+}
+}
