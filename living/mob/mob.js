d78925a578583e41bd7b1d9357a3508b05f8e749
Euan Barclay <Silverswimmer63@gmail.com>
2019-12-04 10:09:29 -0500

    cas ork

--- living/mob/mob.js
+++ living/mob/mob.js
@@ -1,8 +1,19 @@
 class Mob extends Living{
   constructor(name, type, hp, desc, icon, attackDam){
     super(name, type, hp, desc, icon)
-    this._attackDam = attackDam;
+    this._alive = true;
   }
-  get attackDam(){ return this._attackDam; }
-  set attackDam(attackDam){ this._attackDam = attackDam; }
+  get alive(){ return this._alive; };
+  set alive(alive){ this._alive = true; }
+
+  attackDam(){
+    return Utils.randMath(this.damage.min, this.damage.max);
+  }
+  takeDam(damage){
+    this.hp = this.hp - damage;
+    if (this.hp <= 0) {
+      this.alive = false;
+      this.hp = 0;
+    }
+  }
 }
