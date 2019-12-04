7c0dabe70fe20b4f10acf02d0c8ed61adea5482c
Bowen Zheng <bowenzheng2003@gmail.com>
2019-12-04 09:16:08 -0500

    a random function?

--- Bowen.js
+++ Bowen.js
@@ -1,9 +1,6 @@
 console.log("Bowen.js linked");
-// Body list
-
-/*
-Robots (60 HP)
-Knife Roomba (60 HP)
-Self-driving cars (60 HP)
-*/
-// Body list
+  function randMath(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
