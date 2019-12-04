178526189c2e8ee0d6abab6ef363a20a201b88d6
partial <dan.dj@yahoo.com>
2019-12-04 10:50:04 -0500

    cowboi stuff
    im very sure what im doing but this is at the very least a start

--- living/mob/inventory/cowboy/cowboy.js
+++ living/mob/inventory/cowboy/cowboy.js
@@ -3,3 +3,30 @@ class Cowboy extends Inventory{
     super(name, type, hp, desc, icon, attackDam, drop);
   }
 }
+get name(){ return this._name; }
+set name(name){ this._name = name }
+
+get icon(){ return this._icon; }
+set icon(icon){ this._icon = icon; }
+
+get desc(){ return this._desc; }
+set (desc){ this._desc = desc; }
+
+get hp(){ return this._hp; }
+set hp(hp){ this._hp = hp; }
+
+get type(){ return this._type; }
+set type(type){ this._type = type; }
+
+get attackDam(){ return this._attackDam; }
+set attackDam(attackDam){ this._attackDam = attackDam; }
+
+get drop(){ return this._drop; }
+set drop(drop){ this._drop = drop; }
+text(){
+  var retString = this.name + "<br>";
+  retString += this.desc + "<br>";
+  retString += "oh no a " + this.name + " apears what are you gonna do?<br>";
+  // TODO: add the type once we have it all figured out
+  return retString;
+}
