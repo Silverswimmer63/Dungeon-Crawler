df5651c1a554124e9710493ce30a7a6af604574d
Bowen Zheng <bowenzheng2003@gmail.com>
2019-12-04 10:47:20 -0500

    fixed speelinng

--- living/mob/inventory/inventory.js
+++ living/mob/inventory/inventory.js
@@ -1,9 +1,9 @@
 class Inventory extends Mob{
-  constructor(name, type, hp, desc, icon, atackDam, drop){
+  constructor(name, type, hp, desc, icon, attackDam, drop){
     super(name, type, hp, desc, icon, attackDam);
 
   this._drop = drop
-  
+
   }
   get drop() { return this._drop }
   set drop(drop) { this._drop = drop }
--- living/mob/inventory/space/space.js
+++ living/mob/inventory/space/space.js
@@ -1,12 +1,13 @@
 class Space extends Inventory{
-  constructor(name, type, hp, desc, icon, atackDam, drop){
+  constructor(name, type, hp, desc, icon, attackDam, drop){
     super(name, type, hp, desc, icon, attackDam, drop);
   }
+  text(){
+    var retString = "This is " + this.name + "<br>";
+    retString += "This guy is " + this.desc + "<br>";
+    retString += "He has " + this.hp + " HP.<br>";
+    retString += "His attack damage is " + this.attackDam + "<br>"
+    retString += "When you kill him, he drops " + this.drop + "<br>"
+    return retString;
+  }
 }
-text(){
-  var retString = "This is " + this.name + "<br>";
-  retString += "This guy is " + this.desc + "<br>";
-  retString += "He has " + this.hp + " HP.<br>";
-  retString += "His attack damage is " + this.attackDam + "<br>"
-  retString += "WHen you kill him, he drops " + this.drop + "<br>"
-  return retString;
