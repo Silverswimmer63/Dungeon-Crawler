4186c04ba5b13f97d1b3a53f12f7ab73f185ee86
Bowen Zheng <bowenzheng2003@gmail.com>
2019-12-05 09:03:09 -0500

    made too strings for no space no cowboy and no mythological

--- living/mob/noinventory/mythological/no-mythological.js
+++ living/mob/noinventory/mythological/no-mythological.js
@@ -2,4 +2,11 @@ class NoMythological extends Noinventory{
   constructor(name, type, hp, desc, icon, attackDam){
     super(name, type, hp, desc, icon, attackDam);
   }
+  text(){
+    var retString = "This monster is " + this.name + "<br>";
+    retString += "This monster: " + this.desc + "<br>";
+    retString += "The monster has " + this.hp + " HP.<br>";
+    retString += "The monster's attack damage is " + this.attackDam + "<br>"
+    return retString;
+  }
 }
--- living/mob/noinventory/space/no-space.js
+++ living/mob/noinventory/space/no-space.js
@@ -3,10 +3,10 @@ constructor(name, type, hp, desc, icon, attackDam){
   super(name, type, hp, desc, icon, attackDam);
 }
 text(){
-  var retString = "This is " + this.name + "<br>";
-  retString += "This guy is " + this.desc + "<br>";
-  retString += "He has " + this.hp + " HP.<br>";
-  retString += "His attack damage is " + this.attackDam + "<br>"
+  var retString = "This monster is " + this.name + "<br>";
+  retString += "This monster: " + this.desc + "<br>";
+  retString += "The monster has " + this.hp + " HP.<br>";
+  retString += "The monster's attack damage is " + this.attackDam + "<br>"
   return retString;
 }
 }
