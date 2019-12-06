90b9e409e62af164109431fead41420202f09fa7
partial <dan.dj@yahoo.com>
2019-12-06 09:40:26 -0500

    directions
    copy pasted directions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,22 @@
+/*
+Make a function called randomFoe();
+for now, make this in javascript.js
+It should do the following at first implementation -
+1. Take the list of all the opponents.
+2. Find out how long that list is.
+3. Use that number and and randomness to get an index for that list.
+4. Access (but do not remove) the object at that index to make a copy of that foe.
+This means take the template, and use it to make a new monster of the correct class.
+for example, if the class was Cowboy, it should make a new object of Class Cowboy.
+5. Return that object.
+
+You will know this works if the following happens in console when you try to run it:
+
+var foo = randomFoe();
+when you then type foo, you should have a random foe from the list you made.
+*/
 function randomFoe() {
   while(){
-    
+
   }
 }
