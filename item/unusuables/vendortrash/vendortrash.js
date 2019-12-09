27618303e0632dec17f586778cf3bc9ff9ee966a
Euan Barclay <Silverswimmer63@gmail.com>
2019-12-09 10:37:46 -0500

    he

--- item/unusuables/vendortrash/vendortrash.js
+++ item/unusuables/vendortrash/vendortrash.js
@@ -2,4 +2,9 @@ class VendorTrash extends Item {
   constructor(name, icon, desc, value, type){
     super(name, icon, desc, value, type);
   }
+  toString(){
+    var retStr = " you see a " + this.name + ". It is worth" + this.value + ".";
+    retStr += "It can be described as: " + this.desc; + this.class + "it's a"
+    return retStr
+  }
 }
--- item/usuables/weapons/ranged/ranged.js
+++ item/usuables/weapons/ranged/ranged.js
@@ -9,6 +9,11 @@ class Ranged extends Weapon {
 
   text(){
     var retString = super.text();
-    return retstring; 
+    return retstring;
+  }
+  toString(){
+    var retStr = " you see a " + this.name + ". It is worth" + this.value + ".";
+    retStr += "It can be described as: " + this.desc; + this.class + "it's a"
+    return retStr
   }
 }
