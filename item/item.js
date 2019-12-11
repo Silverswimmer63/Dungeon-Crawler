0e5d36314e249c9b377214a287df38833501cd39
Kade <kadethebomb@gmail.com>
2019-12-11 09:17:26 -0500

    css movin ahead

--- item/item.js
+++ item/item.js
@@ -27,7 +27,7 @@ class Item {
   get name(){ return this._name; }
   set name(name){ this._name = name }
 
-  get icon(){ return this._icon; }
+  get icon(){ return "<span class='item'>" + this._icon + "</span>"; }
   set icon(icon){ this._icon = icon; }
 
   get desc(){ return this._desc; }
@@ -40,14 +40,14 @@ class Item {
   set type(type){ this._type = type; }
 
   text(){
-      var retString = this.name + "<br>";
-      reString += this.desc + "<br>";
-      retString += "This looks like it is worth " + this.value + " gold.<br>";
-      // TODO: add the type once we have it all figured out
-      return retString;
-    }
+    var retString = this.name + "<br>";
+    reString += this.desc + "<br>";
+    retString += "This looks like it is worth " + this.value + " gold.<br>";
+    // TODO: add the type once we have it all figured out
+    return retString;
+  }
   toString(){
     return this.icon;
-    }
+  }
 }
 //hi
--- living/mob/mob.js
+++ living/mob/mob.js
@@ -3,15 +3,17 @@ class Mob extends Living{
     super(name, type, hp, desc, icon)
     this._alive = true;
   }
-  get alive(){ return this._alive; };
+  get alive(){ return this._alive; }
+  get range(){ return this._range; }
+  get mob(){ return "<span class='mob'>" + this._mob + "</span>"}
+
   set alive(alive){ this._alive = true; }
+  set range(range){ this._range = range; }
+  set mob(mob){ this._mob = mob; }
 
   attackDam(){
     return Utils.randMath(this.damage.min, this.damage.max);
   }
-
-    get range(){ return this._range; }
-    set range(range){ this._range = range; }
 
   takeDam(damage){
     this.hp = this.hp - damage;
@@ -20,6 +22,7 @@ class Mob extends Living{
       this.hp = 0;
     }
   }
+
   text(){
     if (!alive){
       var retString = "Looks like a dead " + this.name + " Its HP is 0 now.<br>";
