15dd6c9de3c2b101847a0fb0875352a293e0531d
Euan Barclay <Silverswimmer63@gmail.com>
2019-12-12 10:12:41 -0500

    work

--- living/mob/inventory/inventory.js
+++ living/mob/inventory/inventory.js
@@ -9,8 +9,21 @@ class Inventory extends Mob{
   puts an item in an Inventory
   @param item {obj} an object that inherist from class item
   */
+  /* 6 Update Inventory.add(item)
+  For reasons we will see soon, we will need to have this method be able to take individual items or take arrays of 1 or more items. So we must update .get(item)
+  it should check to see IF the parameter (item) is a single object or an array.
+  BECAUSE EVERYTHING IS AN OBJECT in Javascript, it should check to see if it is an array.
+  RESEARCH how to determine if something is an array in javascript
+  IF (????? item == ?????) then it is an array.
+  If this is the case, add it to the _inventory as multiple things.
+  The _inventory is an array as well.
+  The inventory may or may not have items on it already.
+  The end product should be an array that only has items on it.
+  We have talked about a lot of ways to do this.
+  */
   add(item){
    this._inventory.push(item);
+   
   }
 
   /*drop(index)
