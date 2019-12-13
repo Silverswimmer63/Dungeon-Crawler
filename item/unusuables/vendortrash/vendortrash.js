93bdeab51a20313161f9c9a0390871af7bcde46a
Kade <kadethebomb@gmail.com>
2019-12-13 09:06:21 -0500

    push

--- item/unusuables/vendortrash/vendortrash.js
+++ item/unusuables/vendortrash/vendortrash.js
@@ -1,3 +1,10 @@
+/*VendorTrash
+trash items
+@param name {string} the name of the item
+@param icon {string} the singlr character that shows up on the map
+@param desc {string} the description of the item
+@param value {number} the value of the item
+@param type {string} either the name of the collection the item belings to or trash*/
 class VendorTrash extends Item {
   constructor(name, icon, desc, value, type){
     super(name, icon, desc, value, type);
