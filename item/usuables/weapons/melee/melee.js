886973f49ef65f0a3f3635f1ae7675b6d3093b78
Kade <kadethebomb@gmail.com>
2019-12-13 09:12:53 -0500

    weapons

--- item/usuables/weapons/melee/melee.js
+++ item/usuables/weapons/melee/melee.js
@@ -1,3 +1,10 @@
+/*Melee Weapon
+is the basis for all melee weapons
+@param name {string} the name of the item
+@param icon {string} the singlr character that shows up on the map
+@param desc {string} the description of the item
+@param value {int} the value of the item
+@param damage {int} the value of damage a weapon does*/
 class Melee extends Weapon {
   constructor (name, icon, value, desc, damage){
   super (name, icon, value, desc, damage);
