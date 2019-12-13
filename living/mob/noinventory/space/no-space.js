7f0b9628905ad3227378c05d65e356615f258f41
Kade <kadethebomb@gmail.com>
2019-12-13 09:27:04 -0500

    more comments, like one left

--- living/mob/noinventory/space/no-space.js
+++ living/mob/noinventory/space/no-space.js
@@ -1,3 +1,11 @@
+/*Space Inventory
+base class for cowboy mob
+@param name {string} the name of the item
+@param icon {string} the singlr character that shows up on the map
+@param desc {string} the description of the item
+@param attackDam {int} the value of damage a space does
+@param type {string} the type of mob
+@param hp {int} the amount of hp a nob has*/
 class Nospace extends Noinventory {
 constructor(name, type, hp, desc, icon, attackDam){
   super(name, type, hp, desc, icon, attackDam);
