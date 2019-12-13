411cc9ca11fe2cbed3b3842e12ade646b6c7b5f7
Kade <kadethebomb@gmail.com>
2019-12-13 09:09:17 -0500

    armor

--- item/usuables/armor/head/head.js
+++ item/usuables/armor/head/head.js
@@ -1,3 +1,10 @@
+/*Head Armor
+is armor the player or mobs can wear
+@param name {string} the name of the item
+@param icon {string} the single character that shows up on the map
+@param desc {string} the description of the item
+@param value {int} the value of the item
+@param damageresist {int} the value of resistance armor can take*/
 class Head extends Armor {
   constructor (name, icon, value, desc, damageresist){
   super (name, icon, value, desc, damageresist);
