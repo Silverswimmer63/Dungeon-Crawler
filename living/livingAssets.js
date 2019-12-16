d74e8830a600bf9e1df0826a688719205ca278c6
Euan Barclay <Silverswimmer63@gmail.com>
2019-12-16 10:07:12 -0500

    added damage type to creatures

--- living/livingAssets.js
+++ living/livingAssets.js
@@ -5,7 +5,7 @@ const COMPLETE_MYTHOLOGICALIN_LIST= [
     hp: 20,
     desc:"Stone Lion",
     icon:"M",
-    attackDam: {min: 10, max: 15} ,
+    attackDam: {min: 10, max: 15, type:"normal"} ,
     drop: "Leather Armor"
   },
   {
@@ -14,7 +14,7 @@ const COMPLETE_MYTHOLOGICALIN_LIST= [
     hp: 20,
     desc:"Elephant that has powers",
     icon:"M",
-    attackDam: {min: 10, max: 15},
+    attackDam: {min: 10, max: 15, type:"normal"},
     drop: "Chainmail Armor"
   },
   {
@@ -23,14 +23,14 @@ const COMPLETE_MYTHOLOGICALIN_LIST= [
     hp: 20,
     desc:"Half-man, half horse creatures.",
     icon:"M",
-    attackDam: {min: 10, max: 15},
+    attackDam: {min: 10, max: 15, type:"normal"},
     drop: "Bronze Armor",
     range: 0,
   },
   {
     name:"Minitar",
     hp: 70,
-    attackDam:{min:20, max:40},
+    attackDam:{min:20, max:40, type:"normal"},
     icon:"M",
     desc:"He's a sweetheart if you get to know him",
     type:NoMythological,
@@ -38,7 +38,7 @@ const COMPLETE_MYTHOLOGICALIN_LIST= [
   {
     name:"Areas",
     hp: 100,
-    attackDam:{min:30, max:54},
+    attackDam:{min:30, max:54, type:"normal"},
     icon:"A",
     desc:"He's always mad because he has daddy issues",
     type:NoMythological
@@ -49,7 +49,7 @@ const COMPLETE_NN_MYTHOLOGICALININ_LIST= [
   {
     name:"Gremlin",
     hp: 10,
-    attackDam:{min:5, max:10},
+    attackDam:{min:5, max:10, type:"normal"},
     icon:"m",
     desc: "little green monsters as common as rats in the city honestly mostly just an annoyance more than anything",
     type: NoMythological,
@@ -57,7 +57,7 @@ const COMPLETE_NN_MYTHOLOGICALININ_LIST= [
   {
     name:"lemures",
     hp:10,
-    attackDam:{min:5, max:10},
+    attackDam:{min:5, max:10, type:"normal"},
     icon:"m",
     desc:"dark spirits that look like ghost emmos",
     type:NoMythological,
@@ -66,7 +66,7 @@ const COMPLETE_NN_MYTHOLOGICALININ_LIST= [
   {
     name:"Azeban",
     hp: 10,
-    attackDam:{min:5, max:10},
+    attackDam:{min:5, max:10, type:"normal"},
     icon:"m",
     desc:"imagine rocket from guardians of the galaxy but as a poor theff",
     type:NoMythological,
@@ -80,7 +80,7 @@ const COMPLETE_COWBOY_LIST = [
     hp: 40,
     desc: "He wants to take all of you possessions: your money, your clothes, and your mouse",
     icon: "T",
-    attackDam: {max: 30, min:20},
+    attackDam: {max: 30, min:20, type:"normal"},
     drop: "Leather Armor",
     range: 0,
   },
@@ -90,7 +90,7 @@ const COMPLETE_COWBOY_LIST = [
     hp: 40,
     desc: "He may be drunk but he can still kill you because the strength of drunk people is unmatched",
     icon: "T",
-    attackDam: {max: 30, min:20},
+    attackDam: {max: 30, min:20, type:"normal"},
     drop: "Chainmail Armor",
   },
   {
@@ -99,7 +99,7 @@ const COMPLETE_COWBOY_LIST = [
     hp: 40,
     desc: "Will do anything to get anything",
     icon: "T",
-    attackDam: {max: 30, min:20},
+    attackDam: {max: 30, min:20, type:"normal"},
     drop: "Bronze Armor",
   },
   {
@@ -108,7 +108,7 @@ const COMPLETE_COWBOY_LIST = [
     hp: 100,
     desc: "One day from retirment and his first day on the job",
     icon: "T",
-    attackDam: {max: 30, min:20},
+    attackDam: {max: 30, min:20, type:"normal"},
     drop: "",
   },
   {
@@ -117,7 +117,7 @@ const COMPLETE_COWBOY_LIST = [
     hp: 130,
     desc: "Big business can aford big guns.",
     icon: "T",
-    attackDam: {max: 30, min:20},
+    attackDam: {max: 30, min:20, type:"normal"},
     drop: "",
   }
 ]
@@ -129,7 +129,7 @@ const COMPLETE_NOCOWBOY_LIST = [
     hp:30,
     desc:"She's come to take the kids,but you don't have kids so she wants to speak with you're manager, but youre also not imployed so she wants to kill you instead",
     icon:"M",
-    attackDam: {min: 15, max: 30},
+    attackDam: {min: 15, max: 30, type:"normal"},
     range: 0,
   },
   {
@@ -138,14 +138,14 @@ const COMPLETE_NOCOWBOY_LIST = [
     hp:30,
     desc:"I hate sneks",
     icon:"M",
-    attackDam: {min: 15, max: 30},
+    attackDam: {min: 15, max: 30, type:"normal"},
   },
   {
     name:"Angry cow",
     type:Nocowboy,
     hp:30,
     desc:"(A series of angry moos that corilate unto one word)...Moo!",
-    attackDam: {min: 15, max: 30},
+    attackDam: {min: 15, max: 30, type:"normal"},
     icon:"M"
   }
 ]
@@ -157,7 +157,7 @@ const COMPLETE_INVENTORY_SPACE_LIST = [
     hp: 60,
     desc: "All generic futuristic space video games have untamable robots, so why we do the same thing. These are deadly bois that are fun to fight.",
     icon: "$",
-    attackDam: {min: 40, max: 50},
+    attackDam: {min: 40, max: 50, type:"normal"},
     drop: "Chainmail Armor"
   },
   {
@@ -166,7 +166,7 @@ const COMPLETE_INVENTORY_SPACE_LIST = [
     hp: 60,
     desc: "In this world, Roombas are tired of cleaning up things that humans are too lazy to do. Now these bloodthirsty are ready to spill your blood and then clean it up.",
     icon: "$",
-    attackDam: {min: 40, max: 50},
+    attackDam: {min: 40, max: 50, type:"normal"},
     drop: "Copper Armor"
   },
   {
@@ -175,7 +175,7 @@ const COMPLETE_INVENTORY_SPACE_LIST = [
     hp: 60,
     desc: "That classic, beloved Vroom-Vroom noise becomes the stuff of nightmares in this dystopian universe. You better watch out since they're stopping for no one.",
     icon: "$",
-    attackDam: {min: 40, max: 50},
+    attackDam: {min: 40, max: 50, type:"normal"},
     drop: "Bronze Armor"
   },
   {
@@ -183,7 +183,7 @@ const COMPLETE_INVENTORY_SPACE_LIST = [
     hp: 160,
     desc: "Our beloved iPhone and Windows creator is bringing back something more than a new line of the same iPhone and Windows tech support: vengeance and an unquenchable thirst for blood. But mostly vengeance since you're not buying his insanely high priced iPhones or install the Windows 10 when we clearly ask you to update and restart your computer without closing the computer.",
     icon: "$",
-    attackDam: {min: 75, max: 80},
+    attackDam: {min: 75, max: 80, type:"normal"},
     drop: "Silver Armor"
   },
   {
@@ -192,7 +192,7 @@ const COMPLETE_INVENTORY_SPACE_LIST = [
     hp: 130,
     desc: "Cannot actually help you, just keeps you on hold for an hour.",
     icon: "$",
-    attackDam: {min: 35, max: 45},
+    attackDam: {min: 35, max: 45, type:"normal"},
   }
   ]
 
@@ -203,7 +203,7 @@ const COMPLETE_NOSPACE_LIST = [
     hp: 50,
     desc: "Dudes that look like terminator, but not as buff.",
     icon: "$",
-    attackDam: {min: 35, max:45},
+    attackDam: {min: 35, max:45, type:"normal"},
     range: 0,
   },
   {
@@ -212,7 +212,7 @@ const COMPLETE_NOSPACE_LIST = [
     hp: 50,
     desc: "I knew that Alexa would become the supreme leaders someday.",
     icon: "$",
-    attackDam: {min: 35, max:45},
+    attackDam: {min: 35, max:45}, type:"normal",
     range: 0,
   },
   {
@@ -221,7 +221,7 @@ const COMPLETE_NOSPACE_LIST = [
     hp: 50,
     desc: "I imagined him to be a lot bigger in person.",
     icon: "$",
-    attackDam: {min: 35, max: 45},
+    attackDam: {min: 35, max: 45, type:"normal"},
     range: 0,
   },
 ]
