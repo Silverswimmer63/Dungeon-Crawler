d28c54100dc16c5fe07dce5e0422043e2f6f8e2a
Aidan <aidanbab1@gmail.com>
2020-01-13 09:27:05 -0500

    Yee

---
+++ dungeon/Map.js
@@ -0,0 +1,14 @@
+class{
+  constructor(width,height){
+    this._width = this.width;
+    this._height = this.height;
+    this._fill = "#";
+    this._map = this.map;
+  }
+  //getters for the class map no input
+  get width(){return this._width}
+  get height(){return this._height}
+  get fill(){return this._fill}
+  get map(){return this._map}
+
+}
