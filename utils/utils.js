20e9568b5e897703ce3fb1da5bd1a21939f20da8
Kade <kadethebomb@gmail.com>
2020-01-14 10:11:46 -0500

    began work for tomorrow

--- utils/utils.js
+++ utils/utils.js
@@ -12,27 +12,6 @@ class Utils {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-/*
-  1. In Utils.js
-Make a new static method called intCheck(item, call) Here is the documentation for initCheck
-/* intCheck(item, call="Utils.intCheck.js")
-Checks to see if "item" is an integer. Throws a custom error if not.
-@param item: {mixed} an item to be check if is an integer
-@param call: {string} the Class/function/method where the check occured
-@retun {mixed} returns the item unless it is not an integer
-
-2. look up how to make a custom error message in javaScript
-catch and throw error
-3. look up how to check to see if something is an integer in javaScript
-Number.isInteger()
-4. In intCheck(item, call) check to see if the item is an integer. If so, return it.
-
-5. If the item is not an integer, then throw your new Error. The text of this error should be :
-"The method " + call + " expected an integer and received " + item + ".";
-
-6. Test this method.
-*/
-
   static intCheck(item, call="Utils.intCheck.js"){
     if (Number.isInteger(item)) {return item}
     else {
@@ -45,6 +24,14 @@ Number.isInteger()
       return item;
     } else {
       throw new Error("The method " + call + " expected an object and recieved " + item + ".");
+    }
+  }
+
+  static keyCheck(item, key, call="Utils.keyCheck.js"){
+    objCheck();
+    
+    for (var i = 0; i < key.length; i++) {
+
     }
   }
 
