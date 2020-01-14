625fa96a56fe71a197d999ac4421f1a6953ce89b
Aidan <aidanbab1@gmail.com>
2020-01-14 09:12:10 -0500

    Fo real the map works now

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,18 @@
 class Utils {
 
-  static randMath(min, max){
+  static randMath(min, max, call="Utils.randMath"){
+    min = this.intCheck(min,call);
+    max = this.intCheck(min,call);
+    if(min >= max){
+      throw new Error("Min must always be less than max in " + call + ".")
+    }
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-}
+
+
+static intCheck(item, call="Utils.intCheck.js"){
+    if(Number.isInteger(item)){return item;}
+      else{throw new Error( "The method " + call + "and recived " + item + "." )}
+
+    }
+ }
