4b817ed3ed35362f66375f2d428022dd1ced3e75
Kade <kadethebomb@gmail.com>
2020-01-14 10:01:16 -0500

    objCheck and map getters and setters

--- dungeon/map/map.js
+++ dungeon/map/map.js
@@ -1,13 +1,77 @@
+/*
+class Map
+ this class is used to create and generate displays of 2 demensional maps.
+ defult map is filled with unreachable spaces.
+ @param width {int}: width of the map(max x cord)
+ @param height {int}: height of the map(max y cord)
+  */
 class Map{
-  constructor(width, height){
-    this._width = width;
-    this._height = height;
+  constructor(width,height){
+    this._width = Utils.intCheck(width, "Map constructor");
+    this._height = Utils.intCheck(height, "Map constructor");
     this._fill = "#";
-    this._map = {};
+    this._map = this._generateMap();
   }
   //getters
   get width(){ return this._width; }
   get height(){ return this._height; }
   get fill(){ return this._fill; }
-  get map(){ return this._map; }
+  get map(){
+    var retMap = "";
+    retMap += this._drawBorder() + "<br>";
+    for (var i = 1; i <= this.height; i++) {
+      retMap += "|";
+      for (var j = 1; j <= this.width; j++) {
+        retMap += "" + this._map["y"+i]["x"+j];
+      }
+      retMap += "|<br>";
+    }
+    return retMap += this._drawBorder();
+  }
+
+  //setters
+  set width(width){
+    this._width = Utils.intCheck(width, "Map.width");
+    this._map = this._generateMap();
+  }
+  set height(height){
+    this._height = Utils.intCheck(height, "Map.height");
+    this._map = this._generateMap();
+  }
+
+/* _generateMap()
+A method to make a map filled with items of the this._fill value. The "map" is
+an object with a set of objects imbeded within it. All of the top level keys,
+which each owns it's own object, will begin with the letter y (ex y1, y2), and
+so on. The second level objects will be keyed in the same way, but with x
+rather than y for their start. This is done so that we may access the map by
+way of using map.y15.x22 to avoid x and y confusion. The values of the keys in
+the inner objects will be the individual cells of the map.
+*/
+
+  _generateMap(){
+    var map = {};
+    for (var i = 1; i <= this.height; i++) {
+      var key = "y"+i;
+      map[key] = {};
+      for (var j = 1; j <= this.width; j++) {
+        var key2 = "x"+j;
+        map[key][key2] = this.fill;
+      }
+    }
+    return map;//yay
+  }
+
+  /* _drawBorder()
+  Makes a border top or bottom for the map. This border will be in the general
+  design of +---------------+
+  @return {string} a string border
+  */
+  _drawBorder(){
+    var retStr = "+";
+    for (var i = 0; i < this.width; i++) {
+      retStr + "-";
+    }
+    return retStr + "+";
+  }
 }
--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,51 @@
 class Utils {
-
-  static randMath(min, max){
+/*randMath
+@param max {int}: the max you can have
+@param min {int} the min you can have ;
+*/
+  static randMath(min, max, call="Utils.randMath"){
+    min = this.intCheck(min, call);
+    max = this.intCheck(max, call);
+    if (min >= max) {
+      throw new Error("min must always be less then max " + call + ".")
+    }
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+/*
+  1. In Utils.js
+Make a new static method called intCheck(item, call) Here is the documentation for initCheck
+/* intCheck(item, call="Utils.intCheck.js")
+Checks to see if "item" is an integer. Throws a custom error if not.
+@param item: {mixed} an item to be check if is an integer
+@param call: {string} the Class/function/method where the check occured
+@retun {mixed} returns the item unless it is not an integer
+
+2. look up how to make a custom error message in javaScript
+catch and throw error
+3. look up how to check to see if something is an integer in javaScript
+Number.isInteger()
+4. In intCheck(item, call) check to see if the item is an integer. If so, return it.
+
+5. If the item is not an integer, then throw your new Error. The text of this error should be :
+"The method " + call + " expected an integer and received " + item + ".";
+
+6. Test this method.
+*/
+
+  static intCheck(item, call="Utils.intCheck.js"){
+    if (Number.isInteger(item)) {return item}
+    else {
+      throw new Error("The method " + call + " expected an integer and received " + item + ".");
+    }
+  }
+
+  static objCheck(item, call="Utils.objCheck.js"){
+    if ((typeof item == "object") && (item != null)) {
+      return item;
+    } else {
+      throw new Error("The method " + call + " expected an object and recieved " + item + ".");
+    }
+  }
+
 }
