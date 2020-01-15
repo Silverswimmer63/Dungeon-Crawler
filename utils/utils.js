384b0e2deb916b69e4dc033077d3ea66bde1783e
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2020-01-15 10:50:47 -0500

    getting map done

--- utils/utils.js
+++ utils/utils.js
@@ -40,4 +40,61 @@ Number.isInteger()
     }
   }
 
+  /* objCheck(item, call="Utils.objCheck")
+@param item: {mixed} an item to be checked if is an object
+@param call: {string} the Class/function/method where the check occured
+@retun {mixed} returns the item unless it is not an object
+*/
+
+  static objCheck(item, call="Utils.objCheck"){
+    if (( item != null)&&(typeof item == "object")) {
+      return item;
+    }else {
+      throw new Error("The method " + call + " expected an object and received " + item + ".");
+    }
+  }
+
+  /* keyCheck(item, key, call= Utils.keyCheck)
+Performs two actions - 1 checks to see if item is an object. Throws an error
+if not, reporting it from location call. 2 checks to see if the object has
+the key or keys given in the second param. The second param is check to be an
+array or not, if not an array, it is made into a single item array so that it
+can use the same code order. If the object does not, it throws an error.
+@param item: {mixed} an item to be checked if it is an object
+@param key: {mixed} a string or array of strings to be checked as keys in item
+@param call: {string} the Class/function/method where the check occured
+@retun {mixed} returns the item if object with key(s)
+*/
+
+  static keyCheck(item, key, call= "Utils.keyCheck"){
+    this.objCheck(item, call);
+    if (!Array.isArray(key)) {
+      key = [key];
+    }
+    var badKey = [];
+    for (var i = 0; i < key.length; i++) {
+      if (!(key[i] in item)) {
+        badKey.push(key[i]);
+      }
+    }
+    if (badKey.length == 0) {
+      return item;
+    }
+    var errStr = "The method " + call + " expected an object with the key(s) ";
+    for (var i = 0; i < key.length; i++) {
+      errStr += key[i];
+      if ((key.length > 0)&&(i < key.length - 1)) {
+        errStr += ", ";
+      }
+    }
+    errStr += ". It is missing the key(s) ";
+    for (var i = 0; i < badKey.length; i++) {
+      errStr += badKey[i];
+      if ((badKey.length > 0)&&(i < badKey.length - 1)) {
+        errStr += ", ";
+      }
+    }
+    throw new Error(errStr+".")
+  }
+
 }
