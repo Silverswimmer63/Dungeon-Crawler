59cc3aa2c7faee76a195696b114588d22c465a8a
Lilla <dilillahall0802@gmail.com>
2020-01-17 10:32:47 -0600

    hc

--- dungeon/cell/cell.js
+++ dungeon/cell/cell.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*class Cell is a individual space on the grid
 it can be open or closed and it can contain items
 or mobs or the player
@@ -39,56 +38,12 @@ class Cell{
   set inventory(inventory){
     if (this.inventory == 0) {
       this._inventory =inventory;
-=======
-/*class Cell is a individual space on the grid.
-It can be open or closed and it can contain items
-or mobs or the player.
-*/
-class Cell {
-  constructor(){
-    this._image = "#";
-    this._type = "wall"//wall, hall, rooms, border: a wall but a tag
-    this._open = false;// if the cell allows movement
-    this._inventory = [];//items in the cell
-    this._occupied = [];//for livings in the cell
-  }
-
-  get image(){return this._image}
-  set image(image){this._image = this.image}
-
-  get type(){return this._type}
-  set type(type){
-    if (type == "wall" || type == "border") {
-      this._open = false;
-      this._type = type;
-    }else if (type == "room" || type == "hall") {
-      this._open = true;
-      this._type = type;
-    }else {
-      throw new Error("Cell.type expected one of the following: wall, hall, border, or room and got " + type + ".");
-    }
-  }
-
-  get open(){
-    if (this._occupied.length>0) {
-      return false;
-    }else {
-      return this._open;
-    }
-  }
-
-  get inventory(){return this._inventory}
-  set inventory(inventory){
-    if (inventory.length == 0) {
-      this._inventory = inventory;
->>>>>>> origin/JAKE
     }else if (inventory) {
       this._inventory = [];
     }else {
       throw new Error("Cell.inventory can not be used when the inventory is not empty. Please use Cell.add to add to inventory.");
     }
   }
-<<<<<<< HEAD
 }
 /*  for the occupied setter-
 if there is a nonmob in the cell do not allow another nonmob
@@ -98,7 +53,7 @@ if we try to add things we cant, throw an error that reads
 
   set occupied(occupied){
     for (var i = 0; i < occupied.length; i++) {
-      if (!Array.isArray(thing)){thing = [thing]}; {
+      if (!Array.isArray(occupied)){occupied = [occupied]}; {
         occupied = [occupied];
       }
       if (occupied.length > 2) {
@@ -132,46 +87,22 @@ if we try to add things we cant, throw an error that reads
     }
    }
   }
-}  
+}
+set occupied(occupied){this._ocHandler(occupied, "Cell.occupied");}
+/*ocHandler(occupied, call ="Cell.ocHandler")
+This will do all of the interior work for set occupied,
+@param{mixed}occupied an objects*/
+add(occupied){
+  //deternime if its is an object or and array.
+
+  // if it is an array check to see if all are living or all an objects
+  //add tracker flags, toss an error iv both are hit
+  // if anything isnt an item pitch a fit (throw an error)
+
+  // track which one it is.
+  //send to correct function
+}
+_ocHandler()
   toString(){
     return this._image;
   }
-=======
-
-  get occupied(){return this._occupied}
-  set occupied(occupied){
-    if (!Array.isArray(occupied)) {
-      occupied = [occupied];
-    }
-    if (occupied.length > 2) {
-      throw new Error("Cell.occupied expects at most one mob and one nonmob and was given the Array of length " + occupied.length + ".")
-    }
-    for (var i = 0; i < occupied.length; i++) {
-      if (!(occupied[i] instanceof Living)) {
-        throw new Error("Cell.occupied expects at most one mob and one not mon and was given " + occupied[i] + ".")
-      }
-    }
-    for (var i = 0; i < occupied.length; i++) {
-      var mob = false;
-      var nonMob = false;
-      for (var j = 0; j < this._occupied.length; j++) {
-        if (this._occupied[j] instanceof Mob) {mob = true;}// this is setting our trackers
-        if (this._occupied[j] instanceof Nonmob) {nonMob = true;}
-      }
-      // Asumes single item
-
-      if (nonMob == true && occupied[i] instanceof Nonmob) {
-        throw new Error("Cell.occupied - cell already had a nonmob and was given " + occupied[i].name)
-      }else if (mob == true && occupied[i] instanceof Mob) {
-        throw new Error("Cell.occupied - cell already had a mob and was given " + occupied[i].name)
-      }else {
-        this._occupied.push(occupied[i]);
-    }
-  }
-}
-
-  toString(){
-    return this._image;
-  }
-}
->>>>>>> origin/JAKE
--- utils/utils.js
+++ utils/utils.js
@@ -12,19 +12,6 @@ class Utils {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-<<<<<<< HEAD
-/*1. In Utils.js
-Make a new static method called intCheck(item, call) Here is the documentation for initCheck
-/* intCheck(item, call="Utils.intCheck.js")
-wrapper for typeCheck for ints
-@param item: {mixed} an item to be check if is an integer
-@param call: {string} the Class/function/method where the check occured
-@retun {mixed} returns the item unless it is not an integer
-*/
-/*
-2. look up how to make a custom error message in javaScript
-throw new Error("this is not an error")
-=======
 /*
   1. In Utils.js
 Make a new static method called intCheck(item, call) Here is the documentation for initCheck
@@ -36,7 +23,6 @@ Checks to see if "item" is an integer. Throws a custom error if not.
 
 2. look up how to make a custom error message in javaScript
 catch and throw error
->>>>>>> origin/JAKE
 3. look up how to check to see if something is an integer in javaScript
 Number.isInteger()
 4. In intCheck(item, call) check to see if the item is an integer. If so, return it.
@@ -46,45 +32,6 @@ Number.isInteger()
 
 6. Test this method.
 */
-<<<<<<< HEAD
-static intCheck(item, call="Utils.initCheck.js") {
-  if(Number.isInteger(item)) {return item;}
-  else {
-    throw new Error("The method " + call + " expected an integer and received " + item + ".");
-  }
- }
-
-/*1. Check min and max to make sure they are integers. As you've not done this before, I will give you an exact
- example of how this should look -
-min = this.intCheck(min, "Utils.randMath");
-What is going on here is that it will, by default, set min to min. however, if min is not an integer, it will stop everything and throw the error "The method Utils.randMath expected an integer and received (whatever you gave it)", letting us know that we've broken something simply by giving the method something other then a number (like a coordinate)
-do the same as above with max
-
-2. Check to make sure that min is less than max.
-Check if min is greater than or equal to max, if so, throw a new error that reads:
-"Min must always be less than max in Utils.randMath."
-
-3. Test this method
-
-4. Add the parameter call to randMath, give it the default value of "Utils.randMath"
-
-5. Update all the places within the method where Utils.randMath is used as a string with the parameter call.
-
-6 Test this method
-
-7 update the description of the method to include the call parameter.
-*/
-static randMath(min, max, call ="Utils.randMath"){
-  min = this.intCheck(min, call);
-  max = this.intCheck(max, call);
-if (min >= max) {
-  throw new Error ("Min must alway be less the max in" + call+".");
-}
-return Math.Floor(Math.random() *(max - min + 1)) + min;
-}
-
-/* objCheck(item, call="Utils.objCheck")
-=======
 
   static intCheck(item, call="Utils.intCheck.js"){
     if (Number.isInteger(item)) {return item}
@@ -94,22 +41,10 @@ return Math.Floor(Math.random() *(max - min + 1)) + min;
   }
 
   /* objCheck(item, call="Utils.objCheck")
->>>>>>> origin/JAKE
 @param item: {mixed} an item to be checked if is an object
 @param call: {string} the Class/function/method where the check occured
 @retun {mixed} returns the item unless it is not an object
 */
-<<<<<<< HEAD
-static objCheck(item, call="Utils.objCheck"){
-if ((typeof item == "object")&& (item != null )) {
-  return item;
-}  else {
-  throw new Error("The method " + call + " expected an integer and received " + item + ".");
-}
- }
-
-/* keyCheck(item, key, call= Utils.keyCheck)
-=======
 
   static objCheck(item, call="Utils.objCheck"){
     if (( item != null)&&(typeof item == "object")) {
@@ -120,7 +55,6 @@ if ((typeof item == "object")&& (item != null )) {
   }
 
   /* keyCheck(item, key, call= Utils.keyCheck)
->>>>>>> origin/JAKE
 Performs two actions - 1 checks to see if item is an object. Throws an error
 if not, reporting it from location call. 2 checks to see if the object has
 the key or keys given in the second param. The second param is check to be an
@@ -131,38 +65,6 @@ can use the same code order. If the object does not, it throws an error.
 @param call: {string} the Class/function/method where the check occured
 @retun {mixed} returns the item if object with key(s)
 */
-<<<<<<< HEAD
-static keyCheck(item, key, call= "Utils.keyCheck"){
-this.objCheck(item, call);
-if (!Array,isArray(key)) {
-  key = [key];
-}
-var badKey= [];
-  for (var i = 0; i < key.length; i++) {
-if (!(key[i] in item)) {
-  badKey.push(key[i]);
-}
-  }
-  if (badKey.length == 0) {
-    return item;
-  }
-  var errStr = "The method" + call + "expected an object with the key(s)";
-  for (var i = 0; i < key.length; i++) {
-    errStr += key[i];
-    if ((badKey.length > 0 ) && (i < key.length -1)){
-      errStr += ",";
-    }
-    errStr += ". Its is missing key(s)";
-  for (var i = 0; i < badKey.length; i++) {
-    errStr += badKey[i];
-    if ((badKey.length > 0)&& (i < badKey.length - 1)) {
-      errStr += ", ";
-    }
-  }
-    throw new Error(errStr + ".");
-    }
-  }
-=======
 
   static keyCheck(item, key, call= "Utils.keyCheck"){
     this.objCheck(item, call);
@@ -195,5 +97,4 @@ if (!(key[i] in item)) {
     throw new Error(errStr+".")
   }
 
->>>>>>> origin/JAKE
 }
