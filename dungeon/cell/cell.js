fcbc4f6db58412b07bd016bd19e906a08aac4946
Kade <47150589+FuseRush@users.noreply.github.com>
2020-01-17 09:11:11 -0500

    yesterday stuff

--- dungeon/cell/cell.js
+++ dungeon/cell/cell.js
@@ -10,6 +10,72 @@ class Cell {
     this._inventory = [];//items in the cell
     this._occupied = [];//for livings in the cell
   }
+
+  //getters
+  get image(){return this._image}
+  get type(){return this._type}
+  get open(){
+    if (this._occupied.length>0) {
+      return false;
+    }else {
+      return this._open;
+    }
+  }
+  get inventory(){return this._inventory}
+  get occupied(){return this._occupied}
+
+  //setters
+  set image(image){this._image = this.image}
+  set type(type){
+    if (type == "wall" || type == "border") {
+      this._open = false;
+      this._type = type;
+    }else if (type == "room" || type == "hall") {
+      this._open = true;
+      this._type = type;
+    }else {
+      throw new Error("Cell.type expected one of the following: wall, hall, border, or room and got " + type + ".");
+    }
+  }
+  set inventory(inventory){
+    if (inventory.length == 0) {
+      this._inventory = inventory;
+    }else if (inventory) {
+      this._inventory = [];
+    }else {
+      throw new Error("Cell.inventory can not be used when the inventory is not empty. Please use Cell.add to add to inventory.");
+    }
+  }
+  set occupied(occupied){
+    if (!Array.isArray(occupied)) {
+      occupied = [occupied];
+    }
+    if (occupied.length > 2) {
+      throw new Error("Cell.occupied expects at most one mob and one nonmob and was given the Array of length " + occupied.length + ".")
+    }
+    for (var i = 0; i < occupied.length; i++) {
+      if (!(occupied[i] instanceof Living)) {
+        throw new Error("Cell.occupied expects at most one mob and one not mon and was given " + occupied[i] + ".")
+      }
+    }
+    for (var i = 0; i < occupied.length; i++) {
+      var mob = false;
+      var nonMob = false;
+      for (var i = 0; i < this._occupied.length; i++) {
+        if (this._occupied[i] instanceof Mob) {mob = true;}// this is setting our trackers
+        if (this._occupied[i] instanceof Nonmob) {nonMob = true;}
+      }
+      // Asumes single item
+      if (nonMob == true && occupied[i] instanceof Nonmob) {
+        throw new Error("Cell.occupied - cell already had a nonmob and was given" + occupied.name)
+      }else if (mob == true && occupied[i] instanceof Mob) {
+        throw new Error("Cell.occupied - cell already had a mob and was given" + occupied.name)
+      }else {
+        this._occupied.push(occupied[i]);
+    }
+  }
+}
+
   toString(){
     return this._image;
   }
