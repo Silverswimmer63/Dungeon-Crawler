286254c822624b0abda615c63c3f294c3929bc23
Kade <47150589+FuseRush@users.noreply.github.com>
2020-01-27 10:11:25 -0500

    map spawns

--- javascript.js
+++ javascript.js
@@ -1,1 +1,4 @@
-document.getElementById("map").innerHTML = "Map Selected";
+//default javascript generated message for map\
+
+var demo = new Map(70,40)
+document.getElementById("map").innerHTML = demo.map;
