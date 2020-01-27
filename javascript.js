055b0745d50fad27e023e9f627f940df2cae177a
Euan Barclay <Silverswimmer63@gmail.com>
2020-01-27 10:05:37 -0500

    work on map

---
+++ javascript.js
@@ -0,0 +1,6 @@
+//Default javascript generated message for map
+document.getElementById("map").innerHTML = "Map Selected";
+
+/*1. Make a new map in javascript.js
+2. Find a way to display the map using the function above.
+*/
