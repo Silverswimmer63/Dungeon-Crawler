76dddec656e7e8bb224f210db2322206c57a69d0
Lilla <dilillahall0802@gmail.com>
2020-01-28 09:54:37 -0600

    okay

--- javascript.js
+++ javascript.js
@@ -3,5 +3,6 @@
 1. Make a new map in javascript.js
 2. Find a way to display the map using the function above.
 */
-var demo = new Map(70, 40)
+document.getElementById("map").innerHTML = "JS LOADED";
+var demo = new Map(70, 40);
 document.getElementById("map").innerHTML = demo.map;
