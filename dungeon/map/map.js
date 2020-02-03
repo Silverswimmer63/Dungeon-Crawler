d3bbeb876f39ba5e87f78fbee91047da4db60e5a
Lilla <dilillahall0802@gmail.com>
2020-02-03 09:55:28 -0600

    map

--- dungeon/map/map.js
+++ dungeon/map/map.js
@@ -1,170 +1,59 @@
+
 /*
-class Map
- this class is used to create and generate displays of 2 demensional maps.
- defult map is filled with unreachable spaces.
- @param width {int}: width of the map(max x cord)
- @param height {int}: height of the map(max y cord)
-  */
+Class map
+This class is used to create and generate display of two dimwnsional maps.
+@param height(int): height if the map(map y cord)*/
 class Map{
-  constructor(width,height){
-    this._width = Utils.intCheck(width, "map constructor");
-    this._height = Utils.intCheck(height, "map constructor");
+  constructor(width, height){
+    this._width = Utils.intCheck(width, "Map constructor");
+    this._height =  Utils.intCheck(height, "Map constructor");
     this._fill = Cell;
-    this._map = this._generateMap();
-    this._rooms = [];
-    this._roomMin = 3;
-    this._roomMax = 8;
+    this._map =  this._generateMap();
   }
-  /*3. add setters.
-The setters for this function for width and height can be added now. However,
-these will need to be a little more complex than with other setters we have used.
- They will need to do the following -
-1. Check for value inputted being an int, and giving the correct error message
- if it is not.
-2. now that the map has a new width or height, we have to remake it from scratch
-or we will get errors. Remake the this._map.*/
-
-  get roomMin(){return this._roomMin;}
-  set roomMin(roomMin){this._roomMin = Utils.intCheck(this.roomMin);}
-
-  get roomMax(){return this._roomMax;}
-  set roomMax(roomMax){this._roomMax = Utils.intCheck(this.roomMax);}
-
-  get width(){return this._width;}
-  set width(width){
-    this._width = Utils.intCheck(width, "Map.width");
-    this._map = this._generateMap();
-  }
-
+  /*add setters.
+The setters for this function for width and height can be added now. However, these will need
+to be a little more complex than with other setters we have used. They will need to do the following -
+1. Check for value inputted being an int, and giving the correct error message if it is not.
+2. now that the map has a new width or height, we have to remake it from scratch or we will get errors. Remake the this._map.
+*/
+set width(width){
+  this._width = Utils.intCheck(width, "Map.width");
+  this._map =  this._generateMap();
+}
+set height(height){
+  this._height = Utils.intCheck(width, "Map.height");
+  this._map =  this._generateMap();
+}
+  get width(){ return this._width;}
   get height(){return this._height;}
-  set height(height){
-    this._height = Utils.intCheck(height, "Map.height")
-    this._map = this._generateMap();
-  }
-
   get fill(){return this._fill;}
-  set fill(fill){this._fill = Utils.keyCheck(fill,"image","Map.fill")}
-
-  set map(dimensions){
-
-    Utils.keyCheck(dimensions,["width", "height"], "Map.map");
-    Utils.intCheck(dimensions.width,"Map.map");
-    Utils.intCheck(dimensions.height,"Map.map");
-    this._width = dimensions.width;
-    this._height = dimensions.height;
-    this._map = this._generateMap();
-  }
+   set fill(fill){
+     this._fill = Utils.keyCheck(fill, "image", "Map.fill")
+   }
   get map(){
     var retMap = "";
-    retMap += this._drawBorder() + "<br>";
-    for (var i = 1; i <= this.height; i++) {
-      retMap += "|";
-      for (var j = 1; j <= this.width; j++) {
-        retMap += this._map["y"+i]["x"+j];
-      }
-      retMap += "|<br>";
+  retMap += this._drawBorder()+ "<br>";
+  for (var i = 1; i < this.height; i++) {
+    retMap += "|";
+    for (var j = 1; i < this.width; j++) {
+      retMap += ""+ this._map ["y"+i]["x"+j];
     }
-    return retMap += this._drawBorder();
+    retMap += "|<br>";
   }
-
-  get rooms(){return this._rooms;}
-  set rooms(rooms){
-    array =Utils.arrayCheck(rooms,"Map.rooms");
-    if (array.length == 0) {this._rooms = array;}
-    else {
-      let room;
-      for (room of array){
-            Utils.arrayCheck(room, "Map.room individual room.");
-        if (room.length == 0) {throw new Error ("In Map.room: One or more arrays is empty.");
-          let coords;
-          for (coords of room){
-            Utils.keyCheck(coords,"");
-          }
-          }
-        }
-      }
-    }
-    /*    Utils.arrayCheck(rooms,"Map.rooms");
-      if (rooms.length == 0) {
-        this._rooms = rooms;
-      }else {
-        for (var i = 0; i < rooms.length; i++) {
-          Utils.arrayCheck(rooms[i],"Map.rooms - individual room");
-            if (rooms[i].length == 0) {
-              throw new Error("In Map.rooms: One or more room arrays is empty.")
-            }
-          for (var j = 0; j < rooms[i].length; j++) {
-            Utils.keyCheck(rooms[i][j],["x","y"],"Map.rooms - individual cordinate");
-          }
-        }
-      }*/
-
-    /*Then we will update the map to have a setter for map, this will use the two
-     functions above to make sure that the setter is given an object with the keys
-      width and height, and use it to make a new map. After checking the values as well
-      */
-
-  /*addRoom()
-  add room will use the appropiate function in our program to generate a set
-  of coordinates based on our map. It will then go to the map, and update the
-  cells at the correct coordinates to match the room*/
-addRoom(){
-  let coords = Utils.randRoom(this.width, this.height, this.roomMin, this.roomMax);
-  // make a set of coordinates based on the map constraints
-let overlap = false;
-
-for (let i = 0; i < this._rooms.length; i++) {
-if(!overlap) { overlap = Utils.coordCheck(coords, this._rooms[i]); }
-// so we don't lose a true
+  return retMap += this._drawBorder();
 }
-// todo: add a function to pull the outside trim and set to borders
-if(!overlap){
-for (let i = 0; i < coords.length; i++) {
-let cell = this._map["y" + coords[i].y]["x" + coords[i].x];
-cell.image = " "; // todo update type to set the image then have ranked inventy
-cell.type = "room";
+/*Then we will update the map to have a setter for map,
+this will use the two functions above to make sure that the setter
+ is given an object with the keys width and
+height, and use it to make a new map. After checking the values as well */
+set map (dimensions){
+  Utils.keyCheck(dimensions,["width", "height"], "Map.map");
+  Utils.intCheck(dimensions.width,"Map.map");
+  Utils.intCheck(dimensions.height,"Map.map");
+  this._width = dimensions.width;
+  this._height = dimensions.height;
+  this._map =  this._generateMap();
 }
-this._rooms.push(coords);
-}
-}
-/*  var room = Utils.randRoom(this.width, this.height, this.roomMin, this.roomMax);
-  var overlap = false
-  for (let i = 0; i < this._rooms.length; i++) {
-if(!overlap)
-{ overlap = Utils.coordsCheck(coords, this._rooms[i]); }
-// so we don't lose a true
-for (var j = 0; j < room.length; j++) {
-    var key = "x" + room[i].x;
-    var key2 = "y" + room[i].y;
-    var space = this._map[keyY][keyX]
-  }
-}
-  for (var i = 0; i < room.length; i++) {
-if (!overlap) {overlap = Utils.coordCheck(room, this._room[i]);}
-   space.image = " ";
-   space.type = "room";
-}
-this._rooms.push(room);
-}
-*/
-/*let cell = this._map["y"+ coords[i].y]["x"+ coords[i].x];
-if (this._rooms.length != 0) {
-  for (var j = 0; j < this._room.length ; j++) {
-    if ((this._rooms[j].x == cell.x)&&(this._rooms[j].y == cell.y)){
-    }
-  }
-}*/
-/*
-2. add to addRoom functionality to push the room to the _rooms array directly
-
-3. add a step between making the room coordinates and changing the map where you check each room
- in the map array to see if any of them have the same coordinates, and if there is overlap, don't add the room
-
-4. add the correct type of loop structure and other needed items to make said loop stop if the room
-can be added (per 3 above) or keep going if not added
-
-5. modify the structure from 4 above so it stops after a room is added or after 200 tries, whichever comes first.
-  two for lops */
 /* _generateMap()
 A method to make a map filled with items of the this._fill value. The "map" is
 an object with a set of objects imbeded within it. All of the top level keys,
@@ -174,44 +63,57 @@ rather than y for their start. This is done so that we may access the map by
 way of using map.y15.x22 to avoid x and y confusion. The values of the keys in
 the inner objects will be the individual cells of the map.
 */
-
   _generateMap(){
     var map = {};
-    for (var i = 1; i <= this.height; i++) {
-      var key = "y"+i;
-      map[key] = {};
-      for (var j = 1; j <= this.width; j++) {
-        var key2 = "x"+j;
-        map[key][key2] = new this.fill;
-      }
-    }
-    return map;//yay
+for (var i = 1; i <= this._height; i++) {
+  var key = "y"+i;
+  map[key] ={};
+  for (var j = 1; j <= this._width; j++) {
+    var key2 = "x"+j;
+    map[key][key2] = this.fill;
+   }
   }
-
-  /* _drawBorder()
-  Makes a border top or bottom for the map. This border will be in the general
-  design of +---------------+
-  @return {string} a string border
-  */
-  _drawBorder(){
-    var retStr = "+";
-    for (var i = 0; i < this.width; i++) {
-      retStr += "-";
-    }
-    return retStr + "+";
-  }
+  return map; //yay
 }
-/*.1 in Map we are going to add a property for rooms called _rooms this should be a blank array by default
-.2 Add a getter for rooms that returns the array in rooms
-.3 Add a setter for rooms
-.4 Add properties for map for max and min room size, make these 3 and 8 by default. Make getters and setters,
-have the setters check to see if the value is an int in the setter.
-.5 Make a new function in Utils called arrayCheck that does what all the other checkers do, but for arrays
-.6 Add the arrayCheck to the setter for rooms
-.7 make the setter for rooms check to see if the intended value is a blank array [ ]. If not, then check to see
- if each item in the intended item is also an array make the call this time "Map.rooms - individual room"
-.8 for each of the items from #7 above check each of the items inside of it to make sure they are all objects
-with the keys X and Y
-.9 to continue in Map.rooms - for each room make sure it has something in it, otherwise toss the error
-"In Map.rooms: One or more room arrays is empty."
+
+/* _drawBorder()
+Makes a border top or bottom for the map. This border will be in the general
+design of +---------------+
+@return {string} a string border
 */
+ _drawBorder(){
+   var retStr = "+";
+   for (var i = 0; i < this.width; i++) {
+     retStr +="-";
+   }
+   return retStr +"+";
+ }
+/*2. Now we will change the map getter to display the map as the user will see it
+(we won't need to use the getter to access the raw map, as we can do that within the class and for debugging with the _map prop.
+get map should return a string of the map. This string should start with the upper border,
+add a pipe (|) to each side of the map, and end with another copy of the top/bottom border.
+It should add line breaks where appropriate as well.
+
+3. add setters.
+The setters for this function for width and height can be added now. However,
+these will need to be a little more complex than with other setters we have used. They will need to do the following -
+1. Check for value inputted being an int, and giving the correct error message if it is not.
+2. now that the map has a new width or height, we have to remake it from scratch or we will get errors. Remake the this._map.
+*/
+ addRoom(){
+   let coords = Utils.randRoom(this.width, this.height, this.roomMin, this.roomMax);
+   let overlap = false;
+   for (var i = 0; i < this._rooms.length; i++) {
+     if(!overlap) { overlap = Utils.coordCheck(coords, this._rooms[i]); }
+   }
+   if (!overlap){
+     let cell = this._map["y" + coords[i].y]["x" + coords[i].x];
+     cell._image = " ";
+     cell._type = "room;"
+   }
+   this._rooms.push(coords);
+ }
+
+
+
+}
