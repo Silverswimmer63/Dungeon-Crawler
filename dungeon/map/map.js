58496b5862838815b91fc41fc7e55910fa7796e9
Kade <47150589+FuseRush@users.noreply.github.com>
2020-02-12 09:18:39 -0500

    usin bowens

--- dungeon/map/map.js
+++ dungeon/map/map.js
@@ -10,10 +10,11 @@ class Map{
     this._width = Utils.intCheck(width, "map constructor");
     this._height = Utils.intCheck(height, "map constructor");
     this._fill = Cell;
-    this._map = this._generateMap();
     this._rooms = [];
     this._roomMin = 3;
-    this._roomMax = 8;
+    this._roomMax = 10;
+    this._numRooms = 30;
+    this._map = this._generateMap();
   }
 
   get width(){return this._width;}
@@ -81,44 +82,41 @@ Then we will update the map to have a setter for map, this will use the two
   get roomMax(){ return this._roomMax; }
   set roomMax(roomMax){ this._roomMax = Utils.intCheck(roomMax, "Map.roomMax"); }
 
+  get numRooms(){ return this._numRooms; }
+  set numRooms(numRooms){
+    this._numRooms = Utils.intCheck(numRooms, "Map.numRooms");
+  }
+
   /* addRoom()
   add room will use the appropriate functions in our program to generate a set of coordinates based on our map. It will then go to the map,
   and update the cells at the correct coordinates to match the room.
   */
-
-  /* add a step between making the room coordinates and changing the the map
-  where you check each room in the map array to see if any of them have the same
-  coordinates, and if there is overlap, don't add the room
-  border room check
-  place none border room no border but after border room check
-  */
-
-  addRoom(){
-    let num = 0;
+  addRoom(map =this.map){
+    var num = 0;
     while (num < 200) {
       num ++;
-      let overlap = false;
       let border = Utils.randRoom(this.width, this.height, this.roomMin+2, this.roomMax+2); // make a set of coordinates based on the map constraints
-      let smalls = {x:this.width+1,y:this.height+1};
-      let biggy = {x:0,y:0};
+      let overlap = false;
+      var min = {x: this.width +1, y: this.height +1};
+      var max = {x: 0, y:0};
       for (var i = 0; i < border.length; i++) {
-        if (border[i].x < smalls.x) {
-          smalls.x = border[i].x;
+        if (border[i].x < min.x) {
+          min.x = border[i].x;
         }
-        if (border[i].y < smalls.y) {
-          smalls.y = border[i].y;
+        if (border[i].y < min.y) {
+          min.y = border[i].y;
         }
-        if (border[i].x > biggy.x) {
-          biggy.x = border[i].x;
+        if (border[i].x > max.x) {
+          max.x = border[i].x;
         }
-        if (border[i].y > biggy.y) {
-          biggy.y = border[i].y;
+        if (border[i].y > max.y) {
+          max.y = border[i].y;
         }
       }
       let coords = [];
       for (var i = 0; i < border.length; i++) {
         var isBorder = false;
-        if ((border[i].x == biggy.x)||(border[i].y == biggy.y)||(border[i].x == smalls.x)||(border[i].y == smalls.y)) {
+        if ((border[i].x == max.x) || (border[i].y == max.y) || (border[i].x == min.x) || (border[i].y == min.y)) {
           isBorder = true;
         }
         if (!isBorder) {
@@ -129,28 +127,17 @@ Then we will update the map to have a setter for map, this will use the two
         if(!overlap) { overlap = Utils.coordCheck(border, this._rooms[i]); } // so we don't lose a true
       }
       // todo: add a function to pull the outside trim and set to borders
-    if(!overlap){
-      for (let i = 0; i < coords.length; i++) {
-        let cell = this._map["y" + coords[i].y]["x" + coords[i].x];
-        cell.image = " "; // todo update type to set the image then have ranked inventy
-        cell.type = "room";
+      if(!overlap){
+        for (let i = 0; i < coords.length; i++) {
+          let cell = map["y" + coords[i].y]["x" + coords[i].x];
+          cell.open;
+          cell._type = "room";
+        }
+        num = 200;
+        this._rooms.push(coords);
       }
-      num = 200;
-      this._rooms.push(coords);
     }
   }
-}
-
-
-  /* coordCheck(seta, setb)
-  takes 2 arrays of coordinates and checks them to see if there is a coordinate in one that is this in the other. If so it returns a true, if not, it returns a false.
-  */
-  /*
-  3. add a step between making the room coordinates and changing the the map where you check each room in the map array to see
-  if any of them have the same coordinates, and if there is overlap, don't add the room
-  4. add the correct type of loop structure and other needed items to make said loop stop if the room can be added (per 3 above) or keep going if not added
-  5. modify the structure from 4 above so it stops after a room is added or after 200 tries, whichever comes first.
-  */
 
 /* _generateMap()
 A method to make a map filled with items of the this._fill value. The "map" is
@@ -171,7 +158,10 @@ the inner objects will be the individual cells of the map.
         map[key][key2] = new this.fill;
       }
     }
-    return map;
+    for (var i = 0; i < this._numRooms; i++) {
+      this.addRoom(map);//addRoom expects this._map to exist
+    }
+    return map; //this is where we make this._map
   }
 
   /* _drawBorder()
@@ -186,6 +176,5 @@ the inner objects will be the individual cells of the map.
     }
     return retStr += "+";
   }
-
 
 }
