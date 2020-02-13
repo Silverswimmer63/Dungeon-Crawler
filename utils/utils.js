0e282397d4bc544e49e35df251de5e4866963c38
Bowen Zheng <bowenzheng2003@gmail.com>
2020-02-13 10:11:31 -0500

    okey dokey

--- utils/utils.js
+++ utils/utils.js
@@ -96,22 +96,8 @@ class Utils {
     }
   }
 
-  /* coordCheck(seta, setb)
-  takes 2 arrays of coordinates and checks them to see if there is a coordinate
-  in one that is this in the other. If so it returns a true, if not, it returns a false.
-  */
-  static coordCheck(seta, setb){
-    for (var i = 0; i < seta.length; i++) {
-      for (var j = 0; j < setb.length; j++) {
-        if((seta[i].x == setb[j].x) && (seta[i].y == setb[j].y)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  /*randCoord(xMin, xMax, yMin, yMax)
+  /*
+  randCoord(xMin, xMax, yMin, yMax)
   This function will produce an object with the keys of x and y, with values
   betweem xMin - xMax for the x key, & yMin and yMax for the y key
   @param xMin {int}: a number between 1 and xMax
@@ -120,7 +106,7 @@ class Utils {
   @param yMax {int}: a number greater than yMin
   @return {obj}: An obj with x & y keys
   */
-  static randCoord(xMin, xMax, yMin, yMax, call = "Utils.randCoord"){
+  static ranCoord(xMin, xMax, yMin, yMax, call = "Utils.randCoord"){
     var retObj = {};
     retObj.x = this.randMath(xMin, xMax, call);
     retObj.y = this.randMath(yMin, yMax, call);
@@ -130,111 +116,81 @@ class Utils {
   /* randRoom(width, height, roomMin, roomMax)
   Returns an array of 4 coordinate objects
   */
-  static randRoom(width, height, roomMin, roomMax, call="Utils.randRoom"){
-    roomMin -= 1;
-    roomMax -= 1;
+  static randRoom(width, height, roomMin, roomMax){
     var retArray = [];
-    var start = this.randCoord(1, width-roomMin, 1, height-roomMin); // width & height - room Min
-    var stop = this.randCoord(start.x + roomMin, Math.min(start.x + roomMax, width), start.y + roomMin, Math.min(start.y + roomMax, height));
+    roomMin -= 1;
+    roomMax -=1;
+    var start = this.ranCoord(1, width-roomMin, 1, height-roomMin); // width & height - room Min
+    var stop = this.ranCoord(start.x + roomMin, Math.min(start.x + roomMax, width), start.y + roomMin, Math.min(start.y + roomMax, height));
     for (var i = start.x; i <= stop.x; i++) {
       for (var j = start.y; j <= stop.y; j++) {
         retArray.push({x: i, y: j});
       }
     }
     return retArray;
-  }
-
-  /*
-  .1 in Map we are going to add a property for rooms called _rooms this should be
-  a blank array by default
-  .2 Add a getter for rooms that returns the array in rooms
-  .3 Add a setter for rooms
-  .4 Add properties for map for max and min room size, make these 3 and 8 by default.
-  Make getters and setters, have the setters check to see if the value is an int in the setter.
-  .5 Make a new function in Utils called arrayCheck that does what all the other checkers do, but for arrays
-  .6 Add the arrayCheck to the setter for rooms
-  .7 make the setter for rooms check to see if the intended value is a blank array [ ]. If not,
-  then check to see if each item in the intended item is also an array make the call this time "Map.rooms - individual room"
-  .8 for each of the items from #7 above check each of the items inside of it to make sure they are all objects
-  with the keys X and Y
-  .9 to continue in Map.rooms - for each room make sure it has something in it, otherwise toss the error
-  "In Map.rooms: One or more room arrays is empty."
-  */
-  static arrayCheck([], call = "Utils.arrayCheck"){
-    if (( item != null)&&(Array.isArray(item))) {
-      return item;
-    }else {
-      throw new Error("The method " + call + " expected an array and received " + item + ".");
     }
-  }
-
-  /* coordCheck(setA, setB)
-takes 2 arrays of coordinates and checks them to see if there is a coordinate in
-one that is this in the other. If so it returns a true, if not, it returns a false.
-*/
-
-  static coordCheck(setA, setB){
-    for (var i = 0; i < setA.length; i++) {
-    for (var j = 0; j < setB.length; j++) {
-    if((setA[i].x == setB[j].x) && (setA[i].y == setB[j].y)) { return true; }
+    /* coordCheck(seta, setb)
+    takes 2 arrays of coordinates and checks them to see if there is a coordinate in one that is this in the other. If so it returns a true, if not, it returns a false.
+    */
+    static coordCheck(seta, setb){
+      for (var i = 0; i < seta.length; i++) {
+        for (var j = 0; j < setb.length; j++) {
+          if((seta[i].x == setb[j].x) && (seta[i].y == setb[j].y)) {
+            return true;
+          }
+        }
+      }
+      return false;
     }
+
+    static removeBorder(room, width, height){
+      let border = room;
+      var min = {x: width +1, y: height +1};
+      var max = {x: 0, y:0};
+      for (var i = 0; i < border.length; i++) {
+        if (border[i].x < min.x) {
+          min.x = border[i].x;
+        }
+        if (border[i].y < min.y) {
+          min.y = border[i].y;
+        }
+        if (border[i].x > max.x) {
+          max.x = border[i].x;
+        }
+        if (border[i].y > max.y) {
+          max.y = border[i].y;
+        }
+      }
+      let coords = [];
+      for (var i = 0; i < border.length; i++) {
+        var isBorder = false;
+        if ((border[i].x == max.x) || (border[i].y == max.y) || (border[i].x == min.x) || (border[i].y == min.y)) {
+          isBorder = true;
+        }
+        if (!isBorder) {
+          coords.push(border[i]);
+        }
+      }
+      return coords;
+      }
+      /* 3. make a function in utils called cordLine(start, end) that returns a set of coordinates between start (cords) and end (cords)
+
+      Part 3: cordLine :
+      If we had {x:1, y:1} and {x:1, y:6} we would get {x:1 y:2}, {x:1, y:3}.....
+      START AT THE SMALLER ONE
+      "start" and "end" but you need to min and max the values and go from min to max.
+      -Which one we are working on!
+      start at the small to bigger.
+      Let's talk about that for
+      -for(var i = smaller + 1; i < larger; i++){}
+      */
+      static cordLine(start, end){
+        var smaller = Math.min(start, end);
+        var larger = Math.max();
+        var arr = [];
+        for (var i = smaller ++; i < larger; i++) {
+          arr = end["x" + larger[i].x]["y" + larger[i].y];
+        }
+      }
+
   }
-  return false;
-}
-
-
-/*
-1. make a function in utils called removeBorder(room) that does what is being done right now in addRoom to trim the borders.
-
-2. update addRoom to use this function where it is currently doing that.
-
-3. make a function in utils called cordLine(start, end) that returns a set of coordinates between start (cords) and end (cords)
-
-If we had {x:1, y:1} and {x:1, y:6} we would get {x:1 y:2}, {x:1, y:3}.....
-START AT THE SMALLER ONE
-"start" and "end" but you need to min and max the values and go from min to max.
--Which one we are working on!
-start at the small to bigger.
-Let's talk about that for
--for(var i = smaller + 1; i < larger; i++){}
-
-*/
-
-  static removeBorder(border, width, height){
-    let smalls = {x:width+1,y:height+1};
-    let biggy = {x:0,y:0};
-    for (var i = 0; i < border.length; i++) {
-      if (border[i].x < smalls.x) {
-        smalls.x = border[i].x;
-      }
-      if (border[i].y < smalls.y) {
-        smalls.y = border[i].y;
-      }
-      if (border[i].x > biggy.x) {
-        biggy.x = border[i].x;
-      }
-      if (border[i].y > biggy.y) {
-        biggy.y = border[i].y;
-      }
-    }
-    let coords = [];
-    for (var i = 0; i < border.length; i++) {
-      var isBorder = false;
-      if ((border[i].x == biggy.x)||(border[i].y == biggy.y)||(border[i].x == smalls.x)||(border[i].y == smalls.y)) {
-        isBorder = true;
-      }
-      if (!isBorder) {
-        coords.push(border[i]);
-      }
-    }
-    return coords;
-  }
-
-  static coordLine(start, end){
-    let smaller = Math.min();
-    let bigger = Math.max();
-    for(let i = smaller+1; i < bigger.length; i++){
-
-    }
-  }
-}
