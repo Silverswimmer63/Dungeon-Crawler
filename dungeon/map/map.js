6b3351fcee4a642d2299409c19d2aa974ca20946
partial <dan.dj@yahoo.com>
2020-03-03 09:38:51 -0500

    the great culling of my ideas

--- dungeon/map/map.js
+++ dungeon/map/map.js
@@ -4,7 +4,7 @@ class Map
  defult map is filled with unreachable spaces.
  @param width {int}: width of the map(max x cord)
  @param height {int}: height of the map(max y cord)
-  */
+*/
 class Map{
   constructor(width,height){
     this._width = Utils.intCheck(width, "map constructor");
@@ -13,9 +13,21 @@ class Map{
     this._rooms = [];
     this._roomMin = 3;
     this._roomMax = 10;
-    this._numRooms = 30;
+    this._numRooms = 25;
     this._map = this._generateMap();
+    this._halls = [];
   }
+
+  get numRooms(){ return this._numRooms; }
+  set numRooms(numRooms){
+    this._numRooms = Utils.intCheck(numRooms, "Map.numRooms");
+   }
+
+  get roomMax(){ return this._roomMax;}
+  set roomMax(roomMax){this._roomMax = Utils.intCheck(roomMax, "Map.roomMax");}
+
+  get roomMin(){ return this._roomMin;}
+  set roomMin(roomMin){this._roomMin = Utils.intCheck(roomMin, "Map.roomMin");}
 
   get width(){return this._width;}
   set width(width){
@@ -44,11 +56,6 @@ class Map{
     }
     return retMap += this._drawBorder();
   }
-/*
-Then we will update the map to have a setter for map, this will use the two
- functions above to make sure that the setter is given an object with the keys
-  width and height, and use it to make a new map. After checking the values as well
-  */
   set map(dimensions){
     Utils.keyCheck(dimensions,["width", "height"], "Map.map");
     Utils.intCheck(dimensions.width,"Map.map");
@@ -82,26 +89,19 @@ Then we will update the map to have a setter for map, this will use the two
   get roomMax(){ return this._roomMax; }
   set roomMax(roomMax){ this._roomMax = Utils.intCheck(roomMax, "Map.roomMax"); }
 
-  get numRooms(){ return this._numRooms;}
-  set numRooms(numRooms){ this._numRooms = Utils.intCheck(numRooms, "Map.numRooms");}
+  get halls(){ return this._halls }
+  set halls(halls){ throw new Error("Feature not implemented at this time.") }
+
   /* addRoom()
   add room will use the appropriate functions in our program to generate a set of coordinates based on our map. It will then go to the map,
   and update the cells at the correct coordinates to match the room.
   */
-
-  /* add a step between making the room coordinates and changing the the map
-  where you check each room in the map array to see if any of them have the same
-  coordinates, and if there is overlap, don't add the room
-  border room check
-  place none border room no border but after border room check
-  */
-
   addRoom(map=this.map){
     let num = 0;
     while (num < 200) {
       num ++;
-      let overlap = false;
       let border = Utils.randRoom(this.width, this.height, this.roomMin+2, this.roomMax+2); // make a set of coordinates based on the map constraints
+      let overlap = false;
       let coords = Utils.removeBorder(border, this.width, this.height);
       for (let i = 0; i < this._rooms.length; i++) {
         if(!overlap) { overlap = Utils.coordCheck(border, this._rooms[i]); } // so we don't lose a true
@@ -115,26 +115,38 @@ Then we will update the map to have a setter for map, this will use the two
       }
       num = 200;
       this._rooms.push(coords);
-      }
     }
   }
+}
 
-/*
-@param {start}: int
-@param {end}: int
-this takes the distnace between two numbers and returns it.
-*/
+  _makeHall(indexA, indexB){
+    var remover = {roomA: Utils.removeBorder(this.rooms[indexA], this.width, this.height), roomB: Utils.removeBorder(this.rooms[indexB], this.width, this.height)};
+    var setA = {min:{x:this.width,y:this.height}, max:{x:1, y:1}};
+    var setB = {min:{x:this.width,y:this.height}, max:{x:1, y:1}};
+    for (var i = 0; i < remover.roomA.length; i++) {
+      setA.max.x = Math.max(setA.max.x, remover.roomA[i].x);
+      setA.max.y = Math.max(setA.max.y, remover.roomA[i].y);
+      setA.min.x = Math.min(setA.min.x, remover.roomA[i].x);
+      setA.min.y = Math.min(setA.min.y, remover.roomA[i].y);
+    }
+    for (var i = 0; i< remover.roomB.length; i++) {
+      setB.max.x = Math.max(setB.max.x, remover.roomB[i].x);
+      setB.max.y = Math.max(setB.max.y, remover.roomB[i].y);
+      setB.min.x = Math.min(setB.min.x, remover.roomB[i].x);
+      setB.min.y = Math.min(setB.min.y, remover.roomB[i].y);
+    }
+    var aCoords = Utils.randCoord(setA.min.x, setA.max.x, setA.min.y, setA.max.y)
+    var bCoords = Utils.randCoord(setB.min.x, setB.max.x, setB.min.y, setB.max.y)
+    return Utils.hallCords(aCoords, bCoords);
+  }
 
-
-  /* coordCheck(seta, setb)
-  takes 2 arrays of coordinates and checks them to see if there is a coordinate in one that is this in the other. If so it returns a true, if not, it returns a false.
-  */
-  /*
-  3. add a step between making the room coordinates and changing the the map where you check each room in the map array to see
-  if any of them have the same coordinates, and if there is overlap, don't add the room
-  4. add the correct type of loop structure and other needed items to make said loop stop if the room can be added (per 3 above) or keep going if not added
-  5. modify the structure from 4 above so it stops after a room is added or after 200 tries, whichever comes first.
-  */
+  _addHalls(){
+    var shuff = Utils.shuffleIndex(this.rooms)
+    for (var i = 0; i < shuff.length -1; i++) {
+      var hall = this._makeHall(shuff[i], shuff[i+1])
+      this.halls.push(hall);
+    }
+  }
 
 /* _generateMap()
 A method to make a map filled with items of the this._fill value. The "map" is
@@ -155,9 +167,14 @@ the inner objects will be the individual cells of the map.
         map[key][key2] = new this.fill;
       }
     }
+      for (var i = 0; i < this._numRooms; i++) {
+        this.addRoom(map);// addRoom expects this._map tpo exists.
+    }
+    return map;// this is where we make this._map
     for (var i = 0; i < this.numRooms; i++) {
       this.addRoom(map);
     }
+    this._addHalls();
     return map;
   }
 
@@ -173,6 +190,5 @@ the inner objects will be the individual cells of the map.
     }
     return retStr += "+";
   }
-
 
 }
