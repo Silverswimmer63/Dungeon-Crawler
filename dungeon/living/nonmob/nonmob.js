f71773f07260cf9c753a33221a7a8ed3e7a39781
Kade <47150589+FuseRush@users.noreply.github.com>
2020-03-09 09:05:28 -0400

    master pull

---
+++ dungeon/living/nonmob/nonmob.js
@@ -0,0 +1,10 @@
+class Nonmob extends Living{
+    constructor(name, type, hp, desc, icon){
+      super(name, type, hp, desc, icon)
+  }
+}
+// name: this shows the monsters' name.
+// icon: the single character that shows up on the map
+// desc: the description of the item
+// type: this determines what kind the item is. For example, you can see if it is armor, weapon, (vendor trash?) or potions.
+// hp: the amount of the life it has.
